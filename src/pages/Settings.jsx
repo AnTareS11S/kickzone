270daef742fbd6c46dc62739c0549882df13de01
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:37:53 +0100

    add Settings page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,19 @@
+import ChangePassword from '../components/account/ChangePassword';
+import DeleteAccount from '../components/account/DeleteAccount';
+import { Separator } from '../components/ui/separator';
+const Settings = () => {
+  return (
+    <div className='space-y-6'>
+      <div>
+        <h3 className='text-lg font-medium'>Settings</h3>
+        <p className='text-sm text-muted-foreground'>Manage your account</p>
+      </div>
+      <Separator />
+      <ChangePassword />
+      <Separator />
+      <DeleteAccount />
+    </div>
+  );
+};
+
+export default Settings;
