4c8e26695acbbf6778a4aa926e6a3f3b46aa2fb8
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:35:50 +0100

    add columns file to use dataTable

---
+++ src/components/admin/users/columns.jsx
@@ -0,0 +1,68 @@
+import { Button } from '../../ui/button';
+import { CaretSortIcon } from '@radix-ui/react-icons';
+import RoleActions from './RoleActions';
+import DeleteActions from './DeleteActions';
+
+export const columns = [
+  {
+    accessorKey: 'username',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Username
+          <CaretSortIcon className='w-4 h-4 ml-2' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Email
+          <CaretSortIcon className='w-4 h-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className='lowercase'>{row.getValue('email')}</div>;
+    },
+  },
+  {
+    accessorKey: 'role',
+    header: 'Role',
+    cell: ({ row }) => <RoleActions row={row} />,
+  },
+
+  {
+    accessorKey: 'createdAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='text-right'
+        >
+          Created At
+          <CaretSortIcon className='w-4 h-4 ml-2' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const date = new Date(row.original.createdAt);
+      return <div className='text-right'>{date.toLocaleDateString()}</div>;
+    },
+  },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: ({ row }) => <DeleteActions row={row} />,
+  },
+];
