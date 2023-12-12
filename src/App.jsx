8753ec9aaf558681fb6ef2b899d49dd519024524
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:28:06 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import Account from './pages/Account';
+import SettingsLayout from './pages/SettingsLayout';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import AdminRoute from './components/AdminRoute';
+import Users from './pages/Users';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>React App</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/user' element={<SettingsLayout />}>
+          <Route path='/user/profile' element={<Profile />} />
+          <Route path='/user/account' element={<Account />} />
+          <Route path='/user/settings' element={<Settings />} />
+          <Route path='/user/admin' element={<AdminRoute />}>
+            <Route path='/user/admin/users' element={<Users />} />
+          </Route>
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/user' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
