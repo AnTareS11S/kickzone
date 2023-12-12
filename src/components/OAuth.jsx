bcf934c02706b5918d1c1bc77567b19a71295fb6
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:32:03 +0100

    build oauth component

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,46 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { Button } from './ui/button';
+import { Icons } from './ui/icons';
+
+const OAuth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleGoogleAuth = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch('/api/auth/google-auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('Could not sign in with Google', error);
+    }
+  };
+  return (
+    <Button variant='outline' onClick={handleGoogleAuth}>
+      <Icons.google className='mr-2 h-4 w-4' />
+      Google
+    </Button>
+  );
+};
+
+export default OAuth;
