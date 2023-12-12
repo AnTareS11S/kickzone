dd376e1cbd5cf94f178a2f92fee923e79f4d4c6e
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:31:44 +0100

    build ModalDialog

---
+++ src/components/ModalDialog.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../components/ui/alert-dialog';
+import { Button } from '../components/ui/button';
+import { TrashIcon } from '@radix-ui/react-icons';
+
+const ModalDialog = ({ title, description, handleClick, type }) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {type === 'button' ? (
+          <Button
+            variant='outline'
+            className='mt-4 sm:mt-4 bg-red-600 text-white hover:bg-red-700 hover:text-white'
+          >
+            {title}
+          </Button>
+        ) : (
+          <TrashIcon className='w-5 h-5 cursor-pointer' />
+        )}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className='bg-red-600 hover:bg-red-700'
+            onClick={handleClick}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ModalDialog;
