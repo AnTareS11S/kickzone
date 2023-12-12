e6776579ed8181b09a3c492ad3c5718b734b003c
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:34:47 +0100

    add deleteActions

---
+++ src/components/admin/users/DeleteActions.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+
+import ModalDialog from '../../ModalDialog';
+
+import { useNavigate } from 'react-router-dom';
+
+const DeleteActions = ({ row }) => {
+  const id = row.original._id;
+  const navigate = useNavigate();
+
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`/api/admin/users/delete/${id}`, {
+        method: 'DELETE',
+      });
+      if (!res.ok) {
+        throw new Error(data.message || 'Failed to delete user!');
+      }
+      const data = await res.json();
+      navigate(0);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className='flex items-center space-x-4'>
+      <ModalDialog
+        description='Are you sure you want to delete this user?'
+        handleClick={handleDeleteUser}
+      />
+    </div>
+  );
+};
+
+export default DeleteActions;
