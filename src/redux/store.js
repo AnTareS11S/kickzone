c5a57cd8ee055ff6e7a66344ff8dee751541bec3
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-12 19:30:46 +0100

    redux config

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }), // This is to avoid the error: A non-serializable value was detected in an action, in the path: `payload`. Value: [object Promise]
+});
+
+export const persistor = persistStore(store);
