d21e2a8858641b3da73ae37ad993d69671b56021
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-13 18:12:53 +0100

    remove old component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Signup from './pages/Signup';
 import About from './pages/About';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import Account from './pages/Account';
 import SettingsLayout from './pages/SettingsLayout';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import AdminRoute from './components/AdminRoute';
 import Users from './pages/Users';
+import RefereeProfile from './pages/RefereeProfile';
 
 const App = () => {
   return (
@@ -23,7 +23,7 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/user' element={<SettingsLayout />}>
           <Route path='/user/profile' element={<Profile />} />
-          <Route path='/user/account' element={<Account />} />
+          <Route path='/user/referee/profile' element={<RefereeProfile />} />
           <Route path='/user/settings' element={<Settings />} />
           <Route path='/user/admin' element={<AdminRoute />}>
             <Route path='/user/admin/users' element={<Users />} />
