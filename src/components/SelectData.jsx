4d577e5b45a88e0008e5b39d3801dfb8271ab5da
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-15 21:58:25 +0100

    add condition

--- src/components/SelectData.jsx
+++ src/components/SelectData.jsx
@@ -8,8 +8,9 @@ import {
   SelectValue,
 } from './ui/select';
 import { FormControl } from './ui/form';
+import { Input } from './ui/input';
 
-const SelectData = ({ placeholder, items, onChange, defaultValue }) => {
+const SelectData = ({ placeholder, items, onChange, defaultValue, idFlag }) => {
   return (
     <Select onValueChange={onChange} defaultValue={defaultValue}>
       <FormControl>
@@ -19,11 +20,16 @@ const SelectData = ({ placeholder, items, onChange, defaultValue }) => {
       </FormControl>
       <SelectContent>
         <SelectGroup>
-          {items.map((item) => (
-            <SelectItem key={item} value={item}>
-              {item}
-            </SelectItem>
-          ))}
+          {Array.isArray(items) &&
+            items.map((item) => (
+              <SelectItem
+                key={item.split(':')[1]}
+                value={idFlag ? item.split(':')[1] : item.split(':')[0]}
+              >
+                {item.split(':')[0]}
+              </SelectItem>
+            ))}
+          <Input className='hidden' />
         </SelectGroup>
       </SelectContent>
     </Select>
