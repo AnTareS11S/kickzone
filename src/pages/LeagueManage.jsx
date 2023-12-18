ae9179a4040dd20ad79c5e2a37695ed8745caa94
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 14:20:25 +0100

    add league page

---
+++ src/pages/LeagueManage.jsx
@@ -0,0 +1,15 @@
+import { Separator } from '../components/ui/separator';
+
+const TeamManage = () => {
+  return (
+    <div className='space-y-6'>
+      <div>
+        <h3 className='text-lg font-medium'>Manage Leagues</h3>
+        <p className='text-sm text-muted-foreground'>Manage leagues.</p>
+      </div>
+      <Separator />
+    </div>
+  );
+};
+
+export default TeamManage;
