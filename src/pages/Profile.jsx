a9a2503ddf15c509ea57e787833ad95473e76374
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 14:20:12 +0100

    add css classes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { Separator } from '../components/ui/separator';
 
 const Profile = () => {
   return (
-    <div className='space-y-6'>
+    <div className='space-y-6 max-w-screen-md mx-auto'>
       <div>
         <h3 className='text-lg font-medium'>Profile</h3>
         <p className='text-sm text-muted-foreground'>
