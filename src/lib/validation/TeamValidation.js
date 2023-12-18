391f0100dcba8f5207a251814e2bbf1c14c86582
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 21:15:22 +0100

    add edit option

--- src/lib/validation/TeamValidation.js
+++ src/lib/validation/TeamValidation.js
@@ -1,51 +1,51 @@
 import * as z from 'zod';
-export const teamFormSchema = z.object({
-  name: z
-    .string()
-    .min(1, {
-      message: 'Name must be at least 4 characters.',
-    })
-    .max(30, {
-      message: 'Name must not be longer than 30 characters.',
-    })
-    .refine(
-      async (value) => {
-        const isEdit = value !== '';
-        const res = await fetch('/api/team/check', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name: value, isEdit }),
-        });
-        const data = await res.json();
-        return data.success;
+export const teamFormSchema = (isEdit) =>
+  z.object({
+    name: z
+      .string()
+      .min(1, {
+        message: 'Name must be at least 4 characters.',
+      })
+      .max(30, {
+        message: 'Name must not be longer than 30 characters.',
+      })
+      .refine(
+        async (value) => {
+          const res = await fetch('/api/team/check', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: value, isEdit }),
+          });
+          const data = await res.json();
+          return data.success;
+        },
+        {
+          message: 'Team name already exists.',
+        }
+      ),
+
+    yearFounded: z.any().refine(
+      (value) => {
+        // If the value is a string, try to parse it as an integer
+        const parsedValue =
+          typeof value === 'string' ? parseInt(value, 10) : value;
+        // Validate that the parsed value is a positive integer
+        return !isNaN(parsedValue) && parsedValue > 0;
       },
       {
-        message: 'Team name already exists.',
+        message: 'Year founded must be a positive integer',
       }
     ),
-
-  yearFounded: z.any().refine(
-    (value) => {
-      // If the value is a string, try to parse it as an integer
-      const parsedValue =
-        typeof value === 'string' ? parseInt(value, 10) : value;
-      // Validate that the parsed value is a positive integer
-      return !isNaN(parsedValue) && parsedValue > 0;
-    },
-    {
-      message: 'Year founded must be a positive integer',
-    }
-  ),
-  coach: z.string().min(1, {
-    message: 'Coach is required',
-  }),
-  city: z.string().min(1, {
-    message: 'City is required',
-  }),
-  photo: z.string().optional(),
-  bio: z.string().min(1, {
-    message: 'Bio is required',
-  }),
-});
+    coach: z.string().min(1, {
+      message: 'Coach is required',
+    }),
+    city: z.string().min(1, {
+      message: 'City is required',
+    }),
+    photo: z.string().optional(),
+    bio: z.string().min(1, {
+      message: 'Bio is required',
+    }),
+  });
