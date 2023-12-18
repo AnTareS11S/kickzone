3333b2af83e9ff41a96164824547823a75aece04
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 14:19:59 +0100

    add css classes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,7 +3,7 @@ import DeleteAccount from '../components/account/DeleteAccount';
 import { Separator } from '../components/ui/separator';
 const Settings = () => {
   return (
-    <div className='space-y-6'>
+    <div className='space-y-6 max-w-screen-md mx-auto'>
       <div>
         <h3 className='text-lg font-medium'>Settings</h3>
         <p className='text-sm text-muted-foreground'>Manage your account</p>
