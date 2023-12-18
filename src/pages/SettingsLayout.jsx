f175a7ead1bc8887903b8038b140420bd4bcf083
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 14:19:44 +0100

    make responsive layout and add link

--- src/pages/SettingsLayout.jsx
+++ src/pages/SettingsLayout.jsx
@@ -32,44 +32,29 @@ const sidebarNavItems = [
     href: '/user/admin/teams',
     role: 'admin',
   },
+  {
+    title: 'Manage Leagues',
+    href: '/user/admin/leagues',
+    role: 'admin',
+  },
 ];
 
 export default function SettingsLayout() {
   return (
-    <>
-      <div className='md:hidden'>
-        <img
-          src='/examples/forms-light.png'
-          width={1280}
-          height={791}
-          alt='Forms'
-          className='block dark:hidden'
-        />
-        <img
-          src='/examples/forms-dark.png'
-          width={1280}
-          height={791}
-          alt='Forms'
-          className='hidden dark:block'
-        />
+    <div className='space-y-6 p-4 pb-16'>
+      <div className='space-y-0.5'>
+        <h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
+        <p className='text-muted-foreground'>Manage your account settings.</p>
       </div>
-      <div className='hidden space-y-6 p-10 pb-16 md:block'>
-        <div className='space-y-0.5'>
-          <h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
-          <p className='text-muted-foreground'>
-            Manage your account settings and set e-mail preferences.
-          </p>
-        </div>
-        <Separator className='my-6' />
-        <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
-          <aside className='-mx-4 lg:w-1/5'>
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className='flex-1 lg:max-w-2xl'>
-            <Outlet />
-          </div>
+      <Separator className='my-6' />
+      <div className='flex flex-col lg:flex-row '>
+        <aside className='pr-12'>
+          <SidebarNav items={sidebarNavItems} />
+        </aside>
+        <div className='flex-1 max-sm:py-10 max-md:py-10 max-lg:py-10 '>
+          <Outlet />
         </div>
       </div>
-    </>
+    </div>
   );
 }
