f8a92422e59cfa9f385fc4e7f37f3544914635d5
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 21:18:24 +0100

    add LeagueManage page

--- src/pages/LeagueManage.jsx
+++ src/pages/LeagueManage.jsx
@@ -1,6 +1,8 @@
+import LeaguePanel from '../components/admin/leagues/LeaguePanel';
+import { columns } from '../components/admin/leagues/columns';
 import { Separator } from '../components/ui/separator';
 
-const TeamManage = () => {
+const LeagueManage = () => {
   return (
     <div className='space-y-6'>
       <div>
@@ -8,8 +10,9 @@ const TeamManage = () => {
         <p className='text-sm text-muted-foreground'>Manage leagues.</p>
       </div>
       <Separator />
+      <LeaguePanel columns={columns} />
     </div>
   );
 };
 
-export default TeamManage;
+export default LeagueManage;
