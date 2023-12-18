7bc3e30ee45089f3908cb399bbd2cb1e9c524d1f
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-18 14:19:10 +0100

    add fetchCoaches fun

---
+++ src/lib/apiUtils.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+const fetchCoaches = async () => {
+  try {
+    const res = await fetch('/api/admin/coaches');
+    const data = await res.json();
+    return data.map(
+      (coach) => coach.name + ' ' + coach.surname + ':' + coach._id
+    );
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+const fetchTeamDataForTable = async (setTableData, updateSuccess) => {
+  try {
+    const res = await fetch('/api/admin/teams');
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data);
+      return;
+    }
+    setTableData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { fetchCoaches, fetchTeamDataForTable };
