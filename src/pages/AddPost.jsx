80cd8417c9ef9f1a9d4528c6355626a5064436fb
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-20 20:42:06 +0100

    add createPost page

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,12 @@
+import PostForm from '../components/home/posts/PostForm';
+
+const AddPost = () => {
+  return (
+    <>
+      <h1 className='head-text text-dark-2 mb-9 text-left'>Create post</h1>
+      <PostForm />
+    </>
+  );
+};
+
+export default AddPost;
