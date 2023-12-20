1608dc2ebd63eb6e6a9c8ded1e6a46e6c4c63e7b
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-12-20 20:41:40 +0100

    add Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from 'react';
+import PostCard from '../components/home/posts/PostCard';
+import { useSelector } from 'react-redux';
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch('/api/post/all');
+        const data = await res.json();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  console.log(posts.map((post) => post.author._id));
+
+  return (
+    <>
+      <h1 className='head-text text-dark-2 text-left'>Home</h1>
+      <section className='mt-9 flex flex-col gap-10'>
+        {posts.length === 0 ? (
+          <p className='no-result'>No posts found</p>
+        ) : (
+          <>
+            {posts.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                currentUserId={currentUser?._id || ''}
+                parentId={post.parentId}
+                content={post.postContent}
+                author={post.author}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
+  );
 };
 
 export default Home;
