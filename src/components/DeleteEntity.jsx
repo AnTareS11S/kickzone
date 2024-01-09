14af5117157f2a2fe81b2f94ca012ebc0492ae55
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-09 21:47:14 +0100

    add reusable delete component

---
+++ src/components/DeleteEntity.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import { useOnSuccessUpdate } from './hooks/useOnSuccessUpdate';
+import ModalDialog from './ModalDialog';
+
+const DeleteEntity = ({ row, onEntityDelete, apiEndpoint }) => {
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  useOnSuccessUpdate(updateSuccess, () => {
+    onEntityDelete();
+    setUpdateSuccess(false);
+  });
+
+  const handleDeleteEntity = async () => {
+    try {
+      const res = await fetch(`/api/admin/${apiEndpoint}/delete/${row._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message || `Failed to delete ${apiEndpoint}!`);
+      }
+      setUpdateSuccess(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className='flex items-center space-x-4'>
+      <ModalDialog
+        description={`Are you sure you want to delete this ${apiEndpoint}?`}
+        handleClick={handleDeleteEntity}
+      />
+    </div>
+  );
+};
+
+export default DeleteEntity;
