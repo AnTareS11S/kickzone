a138f2ca2d9225206037b92ad4bd74ad83d7fe18
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:40:05 +0100

    change player validation

--- src/lib/validation/PlayerValidation.js
+++ src/lib/validation/PlayerValidation.js
@@ -1,34 +1,68 @@
+/* eslint-disable no-unused-vars */
 import * as z from 'zod';
 
-export const playerFormSchema = z.object({
-  name: z.string().min(1, {
-    message: 'Name is required',
-  }),
-  surname: z.string().min(1, {
-    message: 'Surname is required',
-  }),
-  nationality: z.string().min(1, {
-    message: 'Nationality is required',
-  }),
-  height: z.string().min(1, {
-    message: 'Height is required',
-  }),
-  weight: z.string().min(1, {
-    message: 'Weight is required',
-  }),
-  position: z.string().min(1, {
-    message: 'Position is required',
-  }),
-  number: z.string().min(1, {
-    message: 'Number is required',
-  }),
-  footed: z.string().min(1, {
-    message: 'Footed is required',
-  }),
-  age: z.string().min(1, {
-    message: 'Age is required',
-  }),
-  bio: z.string().min(1, {
-    message: 'Bio is required',
-  }),
-});
+export const playerFormSchema = (isEdit) =>
+  z.object({
+    name: z.string().min(1, {
+      message: 'Name is required',
+    }),
+    surname: z.string().min(1, {
+      message: 'Surname is required',
+    }),
+    nationality: z.string().min(1, {
+      message: 'Nationality is required',
+    }),
+    height: z.any().refine(
+      (value) => {
+        const parsedValue =
+          typeof value === 'string' ? parseInt(value, 10) : value;
+
+        return !isNaN(parsedValue) && parsedValue > 0;
+      },
+      {
+        message: 'Height must be a positive integer',
+      }
+    ),
+    weight: z.any().refine(
+      (value) => {
+        const parsedValue =
+          typeof value === 'string' ? parseInt(value, 10) : value;
+
+        return !isNaN(parsedValue) && parsedValue > 0;
+      },
+      {
+        message: 'Weight must be a positive integer',
+      }
+    ),
+    position: z.string().min(1, {
+      message: 'Position is required',
+    }),
+    number: z.any().refine(
+      (value) => {
+        const parsedValue =
+          typeof value === 'string' ? parseInt(value, 10) : value;
+
+        return !isNaN(parsedValue) && parsedValue > 0;
+      },
+      {
+        message: 'Number must be a positive integer',
+      }
+    ),
+    footed: z.string().min(1, {
+      message: 'Footed is required',
+    }),
+    age: z.any().refine(
+      (value) => {
+        const parsedValue =
+          typeof value === 'string' ? parseInt(value, 10) : value;
+
+        return !isNaN(parsedValue) && parsedValue > 0;
+      },
+      {
+        message: 'Age must be a positive integer',
+      }
+    ),
+    bio: z.string().min(1, {
+      message: 'Bio is required',
+    }),
+  });
