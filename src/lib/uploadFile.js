456deb36dcf0f68f773f0f3f1ae9ed6d9ebdc59c
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:39:35 +0100

    update upload handler

--- src/lib/uploadFile.js
+++ src/lib/uploadFile.js
@@ -1,30 +1,36 @@
 /* eslint-disable no-unused-vars */
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from 'firebase/storage';
-import { app } from '../firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase';
 
-const uploadFile = async (file) => {
-  const storage = getStorage(app);
-  const fileName = new Date().getTime() + '-' + file.name;
-  const storageRef = ref(storage, `avatars/${fileName}`);
+const uploadFile = async (file, setFileCallback, setUploadProgressCallback) => {
+  if (!file) return;
+
+  const name = new Date().getTime() + '-' + file.name;
+  const storageRef = ref(storage, `avatars/${name}`);
   const uploadTask = uploadBytesResumable(storageRef, file);
 
-  return new Promise((resolve, reject) => {
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {},
-      (error) => reject(error),
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          resolve(downloadURL);
-        });
+  uploadTask.on(
+    'state_changed',
+    (snapshot) => {
+      // Handle state change events such as progress, pause, and resume
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadProgressCallback(progress);
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+      console.error('Error during file upload:', error.message);
+    },
+    async () => {
+      // Handle successful uploads on complete
+      try {
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+        // Instead of setting the entire file object, pass only the URL
+        setFileCallback(downloadURL);
+      } catch (error) {
+        console.error('Error getting download URL:', error.message);
       }
-    );
-  });
+    }
+  );
 };
 
 export default uploadFile;
