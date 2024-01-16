e0c63677cbbc31519c26974fe7ee19b4ac0e395e
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:38:43 +0100

    add team details

--- src/components/home/team/TeamDetails.jsx
+++ src/components/home/team/TeamDetails.jsx
@@ -1,16 +1,33 @@
-import { Card, CardContent, CardDescription, CardHeader } from '../../ui/card';
-
 /* eslint-disable react/prop-types */
-const TeamDetails = ({ data }) => {
+import { Link } from 'react-router-dom';
+import { Card, CardContent, CardDescription, CardHeader } from '../../ui/card';
+import { Badge } from '../../ui/badge';
+import Spinner from '../../Spinner';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../ui/tabs';
+import { Separator } from '../../ui/separator';
+
+const profileTabs = [
+  { value: 'results', label: 'Results', icon: '/results.png' },
+  { value: 'matches', label: 'Matches', icon: '/calendar.png' },
+  { value: 'squad', label: 'Squad', icon: '/check.png' },
+];
+
+const TeamDetails = ({ data, isLoading }) => {
+  if (isLoading)
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <Spinner />
+      </div>
+    );
   return (
     <article>
-      <Card className=' mx-auto mt-8 p-8 w-full'>
+      <Card className='mx-auto mt-8 p-8 w-full'>
         <CardHeader className='grid grid-flow-col justify-between mb-4 text-heading3-bold p-1 '>
-          {data.name}
-          {data.logo && (
+          {data?.name}
+          {data?.logo && (
             <img
-              src={data.logo}
-              alt={`Logo of ${data.name}`}
+              src={data?.logo}
+              alt={`Logo of ${data?.name}`}
               className='object-contain w-full h-28 rounded-md'
             />
           )}
@@ -21,20 +38,61 @@ const TeamDetails = ({ data }) => {
           </CardDescription>
           <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4'>
             <div>
-              <p className='text-gray-700 font-semibold'>Founded:</p>
-              <p>{data.yearFounded}</p>
+              <div className='mb-4'>
+                <p className='text-gray-700 font-semibold'>Founded:</p>
+                <p>{data.yearFounded}</p>
+              </div>
+              <div>
+                <p className='text-gray-700 font-semibold'>Stadium:</p>
+                <Link to={`/stadium/${data.stadium?.split(':')[1]}`}>
+                  <Badge variant='outline'>{data.stadium?.split(':')[0]}</Badge>
+                </Link>
+              </div>
             </div>
             <div>
-              <p className='text-gray-700 font-semibold'>Coach:</p>
-              <p>{data.coach}</p>
+              <div>
+                <p className='text-gray-700 font-semibold'>League:</p>
+                <p>{data.league}</p>
+              </div>
+              <div className='mt-4'>
+                <p className='text-gray-700 font-semibold'>Coach:</p>
+                <Link to={`/coach/${data.coach?.split(':')[1]}`}>
+                  <Badge variant='outline'>{data.coach?.split(':')[0]}</Badge>
+                </Link>
+              </div>
             </div>
           </div>
-
-          <div>
-            <p className='text-gray-700 font-semibold'>Stadium:</p>
-            <p>{data.stadium}</p>
-          </div>
         </CardContent>
+        <Separator />
+        <Tabs
+          defaultValue='post'
+          className=' w-full items-center justify-center mt-10 p-2 rounded-full'
+        >
+          <TabsList className=' space-x-4 w-full'>
+            {profileTabs.map((tab) => (
+              <TabsTrigger
+                key={tab.label}
+                value={tab.value}
+                className=' items-center w-full space-x-2 px-4 py-2 text-gray-600 hover:text-gray-800 cursor-pointer '
+              >
+                <img
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className='object-contain'
+                />
+                <p className='max-sm:hidden'>{tab.label}</p>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <TabsContent value='results'></TabsContent>
+          <TabsContent value='matches'>
+            <div className=''>
+              <p>Matches</p>
+            </div>
+          </TabsContent>
+        </Tabs>
       </Card>
     </article>
   );
