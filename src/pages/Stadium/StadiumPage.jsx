d356253e9f68c080a7b57fdd34dfd6aac769c25c
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:43:22 +0100

    create stadium details page

---
+++ src/pages/Stadium/StadiumPage.jsx
@@ -0,0 +1,14 @@
+const StadiumPage = () => {
+  return (
+    <div>
+      <img
+        src={
+          'https://firebasestorage.googleapis.com/v0/b/futbolistapro.appspot.com/o/avatars%2Fblank-profile-picture-973460_960_720.webp?alt=media&token=5779eb88-d84b-46f3-bef6-3c2648a8fc9c'
+        }
+        className='w-40 h-40'
+      />
+    </div>
+  );
+};
+
+export default StadiumPage;
