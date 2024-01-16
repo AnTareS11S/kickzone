03b2f507a488247c4b65b024e9d5d6ba409af761
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:40:17 +0100

    change user validation

--- src/lib/validation/UsersValidation.js
+++ src/lib/validation/UsersValidation.js
@@ -16,9 +16,15 @@ export const usersFormSchema = z.object({
     .max(30, {
       message: 'Surname must not be longer than 30 characters.',
     }),
-  birthDate: z.string(),
-  country: z.string(),
-  city: z.string(),
+  birthDate: z.string().min(1, {
+    message: 'Birth Date is required',
+  }),
+  nationality: z.string().min(1, {
+    message: 'Nationality is required',
+  }),
+  city: z.string().min(1, {
+    message: 'City is required',
+  }),
   photo: z.string().optional(),
   bio: z.string().min(1, {
     message: 'Bio is required',
