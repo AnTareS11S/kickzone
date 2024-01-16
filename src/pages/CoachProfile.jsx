22dcfc68066d9c465db603f57075568d665f2b36
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-16 20:40:38 +0100

    remove fetch data

--- src/pages/CoachProfile.jsx
+++ src/pages/CoachProfile.jsx
@@ -1,28 +1,11 @@
 import CoachForm from '../components/coach/CoachForm';
 import { Separator } from '../components/ui/separator';
-import { useEffect, useState } from 'react';
+
 import { useSelector } from 'react-redux';
 
 const CoachProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  const [coach, setCoach] = useState(null);
 
-  useEffect(() => {
-    const getCoach = async () => {
-      try {
-        const res = await fetch(`/api/coach/get/${currentUser._id}`);
-        if (!res.ok) {
-          throw new Error(data.message || 'Failed to fetch data!');
-        }
-        const data = await res.json();
-
-        setCoach(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getCoach();
-  }, [currentUser._id]);
   return (
     <div className='space-y-6'>
       <div>
@@ -32,7 +15,7 @@ const CoachProfile = () => {
         </p>
       </div>
       <Separator />
-      <CoachForm currentUser={currentUser} coachData={coach} />
+      <CoachForm currentUser={currentUser} />
     </div>
   );
 };
