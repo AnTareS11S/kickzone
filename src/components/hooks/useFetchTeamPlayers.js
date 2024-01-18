8f98bf3195c0b85f27dcc6a0f16371588b6a3fa9
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-18 21:08:42 +0100

    create custom hook

---
+++ src/components/hooks/useFetchTeamPlayers.js
@@ -0,0 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { useFetchCoachById } from './useFetchCoachById';
+export const useFetchTeamPlayers = () => {
+  const coach = useFetchCoachById();
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchTeamPlayers = async () => {
+    try {
+      if (!coach?.currentTeam) {
+        return;
+      }
+      setLoading(true);
+      const res = await fetch(
+        `/api/player/get-wanted-team/${coach?.currentTeam}`
+      );
+      if (!res.ok) {
+        throw new Error('Failed to fetch players data!');
+      }
+      const data = await res.json();
+
+      setPlayers(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    fetchTeamPlayers();
+  }, [coach?.currentTeam]);
+
+  return players;
+};
