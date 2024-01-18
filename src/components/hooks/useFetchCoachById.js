b1b3cde326e4f75c27d4c6427769a3fd10ae6bae
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-01-18 21:08:28 +0100

    create custom hook

---
+++ src/components/hooks/useFetchCoachById.js
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export const useFetchCoachById = () => {
+  const { currentUser } = useSelector((state) => state?.user);
+  const [coach, setCoach] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoachById = async () => {
+    try {
+      if (!currentUser?._id) {
+        return;
+      }
+      setLoading(true);
+      const res = await fetch(`/api/coach/get/${currentUser?._id}`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch coach data!');
+      }
+      const data = await res.json();
+      setCoach(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    fetchCoachById();
+  }, [currentUser]);
+
+  return coach;
+};
