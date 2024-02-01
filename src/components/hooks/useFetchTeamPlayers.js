23dbe190f24d015d627b5b252f489f38ba3fece6
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:23:01 +0100

    change paths

--- src/components/hooks/useFetchTeamPlayers.js
+++ src/components/hooks/useFetchTeamPlayers.js
@@ -1,26 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
-import { useFetchCoachById } from './useFetchCoachById';
-export const useFetchTeamPlayers = () => {
-  const coach = useFetchCoachById();
+
+export const useFetchTeamPlayers = (id) => {
   const [players, setPlayers] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const fetchTeamPlayers = async () => {
     try {
-      if (!coach?.currentTeam) {
-        return;
+      if (!id) {
+        return null;
       }
       setLoading(true);
-      const res = await fetch(
-        `/api/player/get-wanted-team/${coach?.currentTeam}`
-      );
+      const res = await fetch(`/api/admin/team-player/${id}`);
       if (!res.ok) {
         throw new Error('Failed to fetch players data!');
       }
       const data = await res.json();
 
-      setPlayers(data);
+      setPlayers(
+        data.map(
+          (player) => player.name + ' ' + player.surname + ':' + player._id
+        )
+      );
     } catch (error) {
       console.log(error);
     } finally {
@@ -32,7 +33,7 @@ export const useFetchTeamPlayers = () => {
       return;
     }
     fetchTeamPlayers();
-  }, [coach?.currentTeam]);
+  }, [id]);
 
   return players;
 };
