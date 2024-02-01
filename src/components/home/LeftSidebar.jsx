ec5d01946d71780a125b2c37138bcb2c107464b3
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:16:47 +0100

    upadate link on leftSidebar

--- src/components/home/LeftSidebar.jsx
+++ src/components/home/LeftSidebar.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 const sidebarLinks = [
   {
@@ -29,15 +31,122 @@ const sidebarLinks = [
     label: 'Leagues',
   },
   {
-    imgURL: '/user.svg',
-    route: '/profile/:id',
-    label: 'Profile',
+    imgURL: '/training.png',
+    route: '/training',
+    label: 'Training ',
+  },
+  {
+    imgURL: '/dashboard.png',
+    route: '/dashboard/referee',
+    label: 'Dashboard ',
+  },
+  {
+    imgURL: '/dashboard.png',
+    route: '/dashboard/admin',
+    label: 'Dashboard ',
+  },
+  {
+    imgURL: '/dashboard.png',
+    route: '/dashboard/coach',
+    label: 'Dashboard ',
   },
 ];
+
+const SidebarLink = ({ link, isActive, onClick }) => (
+  <Link
+    to={link.route}
+    key={link.label}
+    onClick={onClick}
+    className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+  >
+    <img
+      src={link.imgURL}
+      alt={link.label}
+      width={24}
+      height={24}
+      className='text-dark-2'
+    />
+    <p className='text-dark-2 max-lg:hidden'>{link.label}</p>
+  </Link>
+);
 
 const LeftSidebar = () => {
   const { pathname } = useLocation();
   const { currentUser } = useSelector((state) => state.user);
+  const [player, setPlayer] = useState({});
+
+  useEffect(() => {
+    const getPlayer = async () => {
+      try {
+        if (!currentUser?._id) {
+          return;
+        }
+
+        const res = await fetch(`/api/player/get/${currentUser?._id}`);
+        if (!res.ok) {
+          throw new Error(data.message || 'Failed to fetch data!');
+        }
+        const data = await res.json();
+        setPlayer(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPlayer();
+  }, [currentUser?._id]);
+
+  const isAdminOrCoachOrReferee =
+    currentUser && ['admin', 'coach', 'referee'].includes(currentUser?.role);
+
+  const isCoachWithTeam =
+    (currentUser && currentUser?.role === 'coach') ||
+    player?.currentTeam !== undefined;
+
+  const canShowCreatePostLink = () =>
+    (currentUser && isAdminOrCoachOrReferee) ||
+    (currentUser && currentUser.role === 'coach' && isCoachWithTeam);
+
+  const canShowTrainingLink = () =>
+    currentUser && (currentUser.role === 'coach' || isCoachWithTeam);
+
+  const canShowActivityLink = () =>
+    currentUser &&
+    ['admin', 'coach', 'player', 'referee'].includes(currentUser.role);
+
+  const shouldRenderLink = (link) => {
+    const userRole = currentUser?.role;
+
+    if (link.route === '/post/create' && canShowCreatePostLink()) {
+      return true;
+    }
+
+    if (link.route === '/training' && canShowTrainingLink()) {
+      return true;
+    }
+
+    if (link.route === '/activity' && canShowActivityLink()) {
+      return true;
+    }
+
+    if (link.route === '/leagues') {
+      return true;
+    }
+
+    if (link.route === '/dashboard/referee' && userRole === 'referee') {
+      return true;
+    }
+
+    if (link.route === '/dashboard/admin' && userRole === 'admin') {
+      return true;
+    }
+
+    if (link.route === '/dashboard/coach' && userRole === 'coach') {
+      return true;
+    }
+
+    return ['/', '/search'].includes(link.route);
+  };
 
   return (
     <section className='custom-scrollbar leftsidebar'>
@@ -45,32 +154,18 @@ const LeftSidebar = () => {
         {sidebarLinks.map((link) => {
           const isActive = pathname === link.route;
 
-          // Check if the link should be visible based on the user's role
-          const shouldShowLink =
-            link.label !== 'Create Post' ||
-            (currentUser &&
-              ['referee', 'admin', 'coach'].includes(currentUser.role));
-
-          return shouldShowLink ? (
-            <Link
-              to={
-                link.label === 'Profile'
-                  ? `/profile/${currentUser?._id}`
-                  : link.route
-              }
-              key={link.label}
-              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
-            >
-              <img
-                src={link.imgURL}
-                alt={link.label}
-                width={24}
-                height={24}
-                className='text-dark-2'
+          if (shouldRenderLink(link)) {
+            return (
+              <SidebarLink
+                key={link.label}
+                link={link}
+                isActive={isActive}
+                onClick={() => {}}
               />
-              <p className='text-dark-2 max-lg:hidden'>{link.label}</p>
-            </Link>
-          ) : null;
+            );
+          }
+
+          return null;
         })}
       </div>
     </section>
