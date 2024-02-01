d5d682c84074c9cc84bdbb9a7b93df5ccce4ba59
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:43:01 +0100

    add more complex code

--- src/pages/Referee/RefereeDashboard.jsx
+++ src/pages/Referee/RefereeDashboard.jsx
@@ -1,17 +1,26 @@
-import PlayerStats from '../../components/referee/PlayerStats';
-import Results from '../../components/referee/Results';
-import Schedule from '../../components/referee/Schedule';
+import AdminCard from '../../components/admin/AdminCard';
 import { Separator } from '../../components/ui/separator';
+import { useLocation } from 'react-router-dom';
 
 const RefereeDashboard = () => {
+  const pathname = useLocation().pathname.split('/')[4];
   return (
     <>
       <div className='text-heading2-bold mb-4'>Referee Dashboard</div>
       <Separator />
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-        <Schedule />
-        <Results />
-        <PlayerStats />
+        <AdminCard
+          title='Schedule'
+          linkTo={`/dashboard/referee/league/schedule/${pathname}`}
+        />
+        <AdminCard
+          title='Results'
+          linkTo={`/dashboard/referee/league/results/${pathname}`}
+        />
+        <AdminCard
+          title='Player Stats'
+          linkTo={`/dashboard/referee/league/player-stats/${pathname}`}
+        />
       </div>
     </>
   );
