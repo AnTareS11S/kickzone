5c63b337c44b6dcc7f639a7b54228b9a1aa7fc8e
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:25:24 +0100

    add TrainingDetails component

---
+++ src/pages/Training/TrainingDetails.jsx
@@ -0,0 +1,272 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import Spinner from '../../components/Spinner';
+import { Card, CardContent, CardHeader } from '../../components/ui/card';
+import { Checkbox } from '../../components/ui/checkbox';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '../../components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from '../../components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { useFetchPlayerById } from '../../components/hooks/useFetchPlayerById';
+import { useNavigate } from 'react-router-dom';
+import CrudPanel from '../../components/CrudPanel';
+import DeleteEntity from '../../components/DeleteEntity';
+import { Separator } from '../../components/ui/separator';
+import { useFetchTeamPlayers } from '../../components/hooks/useFetchTeamPlayers';
+import { useFetchCoachByUserId } from '../../components/hooks/useFetchCoachByUserId';
+
+const formSchema = z.object({
+  attendance: z.boolean(),
+});
+
+const playerFormSchema = () => {
+  return z.object({
+    player: z.string().min(1, {
+      message: 'Player is required',
+    }),
+  });
+};
+
+const columns = [
+  {
+    name: 'No.',
+    selector: (row, index) => index + 1,
+    grow: 0,
+  },
+  {
+    name: 'Player',
+    selector: (row) => row.name + ' ' + row.surname,
+    sortable: true,
+  },
+];
+
+const TrainingDetails = () => {
+  const [training, setTraining] = useState({});
+  const [loading, setLoading] = useState(false);
+  const player = useFetchPlayerById();
+  const coach = useFetchCoachByUserId();
+  const players = useFetchTeamPlayers(coach?.currentTeam);
+  const navigate = useNavigate();
+  const pathname = window.location.pathname.split('/')[2];
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      attendance: false,
+    },
+  });
+
+  const getTraining = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`/api/admin/training/get/${pathname}`);
+      if (!res.ok) {
+        throw new Error(data.message || 'Failed to fetch data!');
+      }
+      const data = await res.json();
+      setTraining(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    getTraining();
+  }, [pathname]);
+
+  useEffect(() => {
+    form.setValue('attendance', training?.participants?.includes(player?._id));
+  }, [training?.participants?.includes(player?._id)]);
+
+  const onSubmit = async (data) => {
+    try {
+      const res = await fetch(`/api/admin/training/attendace/${pathname}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...data,
+          playerId: player?._id,
+        }),
+      });
+      if (!res.ok) {
+        throw new Error('Failed to submit data!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fields = [
+    {
+      id: 'player',
+      label: 'Player',
+      type: 'select',
+      name: 'player',
+      items: players,
+      placeholder: 'Select a Player',
+      idFlag: true,
+    },
+  ];
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div
+        className='cursor-pointer mb-2 inline-flex items-center justify-center bg-primary-500 hover:bg-purple-500 text-white font-bold py-1 px-3 rounded'
+        onClick={() => navigate(-1)}
+      >
+        <span className='mr-1'>&#8592;</span> Back
+      </div>
+      <div className=' mt-8'>
+        <Card className='p-2 shadow-md space-y-6 w-full bg-slate-400'>
+          <CardHeader className='text-white py-2'>
+            <div className='text-heading3-bold font-semibold text-center'>
+              {training?.name}
+            </div>
+            <div className='text-gray-800 text-center mt-2'>
+              {new Date(training?.trainingDate).toLocaleDateString('en-GB', {
+                day: 'numeric',
+                month: 'long',
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+              <br />
+              Duration: {training?.duration} minutes
+              <br />
+              Location: {training?.location}
+            </div>
+          </CardHeader>
+          {training?.isActive && player && (
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-4'
+              >
+                <div className='flex items-center justify-center'>
+                  <FormField
+                    control={form.control}
+                    name='attendance'
+                    render={({ field }) => (
+                      <FormItem className='flex items-center gap-2 justify-center text-gray-900'>
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        Mark attendance and submit
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className='flex items-center justify-center'>
+                  <Button
+                    type='submit'
+                    className='bg-primary-500 hover:bg-purple-500'
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          )}
+        </Card>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4'>
+          {training?.trainingType && (
+            <Card className='p-6 shadow-md space-y-6'>
+              <CardHeader>
+                <p className='text-heading4-medium font-semibold text-center'>
+                  Training Type
+                </p>
+              </CardHeader>
+              <CardContent>
+                <p className='text-gray-800'>{training?.trainingType?.name}</p>
+              </CardContent>
+            </Card>
+          )}
+          {training?.trainingType?.description && (
+            <Card className='p-6 shadow-md space-y-6'>
+              <CardHeader>
+                <p className='text-heading4-medium font-semibold text-center'>
+                  Type Description
+                </p>
+              </CardHeader>
+              <CardContent>
+                <p className='text-gray-800'>
+                  {training?.trainingType?.description}
+                </p>
+              </CardContent>
+            </Card>
+          )}
+          {training?.notes && (
+            <Card className='p-6 shadow-md space-y-6'>
+              <CardHeader>
+                <p className='text-heading4-medium font-semibold text-center'>
+                  Coach Notes
+                </p>
+              </CardHeader>
+              <CardContent>
+                <p className='text-gray-800'>{training?.notes}</p>
+              </CardContent>
+            </Card>
+          )}
+
+          {training?.equipment && (
+            <Card className='p-6 shadow-md space-y-6'>
+              <CardHeader>
+                <p className='text-heading4-medium font-semibold text-center'>
+                  Required Equipment
+                </p>
+              </CardHeader>
+              <CardContent>
+                <p className='text-gray-800'>{training?.equipment}</p>
+              </CardContent>
+            </Card>
+          )}
+        </div>
+        <Separator className='my-5' />
+        {!player && training?.participants && (
+          <>
+            <div className='flex items-center justify-center'>
+              <h3 className='text-heading3-bold font-medium'>Participants</h3>
+            </div>
+            <CrudPanel
+              apiPath='participants'
+              columns={columns}
+              fields={fields}
+              title='Participant'
+              objectId={pathname}
+              onDeleteComponent={DeleteEntity}
+              formSchema={playerFormSchema}
+              defaultValues={{ player: '' }}
+            />
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default TrainingDetails;
