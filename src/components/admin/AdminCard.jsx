e3d12f601f1be135b6042231ad0d6473ddbab1a0
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:15:22 +0100

    create adminCard

---
+++ src/components/admin/AdminCard.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+import { Card, CardHeader, CardTitle } from '../ui/card';
+
+const AdminCard = ({ title, linkTo }) => {
+  return (
+    <article className='flex flex-wrap gap-7 mt-9'>
+      <Card className='w-[250px] h-[100px] max-sm:w-full'>
+        <Link to={linkTo}>
+          <CardHeader className='items-center pt-10'>
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+        </Link>
+      </Card>
+    </article>
+  );
+};
+
+export default AdminCard;
