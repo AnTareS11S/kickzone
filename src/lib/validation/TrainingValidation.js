233b7161a2288b3b90f2b930cf5cfa3458a67532
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-02-01 18:23:18 +0100

    add training validation

---
+++ src/lib/validation/TrainingValidation.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import * as z from 'zod';
+
+export const trainingValidationSchema = () =>
+  z.object({
+    name: z.string().min(1, {
+      message: 'Name is required',
+    }),
+    trainingDate: z.coerce.date().refine(
+      (val) => {
+        return val.toLocaleDateString() >= new Date().toLocaleDateString();
+      },
+      {
+        message: 'Training date must be in the future or today',
+      }
+    ),
+    duration: z.coerce
+      .number()
+      .min(1, {
+        message: 'Duration is required',
+      })
+      .max(999, {
+        message: 'Duration must be less than 999',
+      }),
+    description: z.string().optional(),
+    trainingType: z.string().min(1, {
+      message: 'Type is required',
+    }),
+
+    location: z.string().min(1, {
+      message: 'Location is required',
+    }),
+    notes: z.string().optional(),
+    equipment: z.string().optional(),
+  });
+
+export const trainingTypeValidationSchema = () =>
+  z.object({
+    name: z.string().min(1, {
+      message: 'Name is required',
+    }),
+    description: z.string().min(1, {
+      message: 'Description is required',
+    }),
+  });
