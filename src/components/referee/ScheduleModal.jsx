4cd7f415c98100d0a45181387326c87462cd2f72
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-03-13 15:16:15 +0100

    add scheduleModal component

---
+++ src/components/referee/ScheduleModal.jsx
@@ -0,0 +1,140 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import FormArea from '../FormArea';
+import { Button } from '../ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
+
+import { useForm } from 'react-hook-form';
+import { Form } from '../ui/form';
+
+const schema = z.object({
+  homeTeam: z.string().min(1, { message: 'Home team is required' }),
+  awayTeam: z.string().min(1, { message: 'Away team is required' }),
+  startDate: z.coerce
+    .date()
+    .refine(
+      (val) => val.toLocaleDateString() >= new Date().toLocaleDateString(),
+      {
+        message: 'Date must be in the future or today',
+      }
+    ),
+});
+
+const ScheduleModal = ({ match, teams }) => {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      homeTeam: match?.homeTeam || '',
+      awayTeam: match?.awayTeam || '',
+      startDate: match?.startDate || '',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (data) => {
+    const updatedData = {
+      ...data,
+      homeTeam: data.homeTeam.split(':')[1]
+        ? data.homeTeam.split(':')[1]
+        : data.homeTeam,
+      awayTeam: data.awayTeam.split(':')[1]
+        ? data.awayTeam.split(':')[1]
+        : data.awayTeam,
+      startDate: new Date(data.startDate).toISOString(),
+    };
+
+    try {
+      const res = await fetch(`/api/referee/edit-match/${match?.matchId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      });
+
+      if (!res.ok) {
+        throw new Error('Error updating match');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='secondary' className='w-32'>
+          Edit Match
+        </Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[925px] h-[300px]'>
+        <DialogHeader>
+          <DialogTitle>Edit Match</DialogTitle>
+          <DialogDescription>
+            Make changes to this match here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className='flex flex-col gap-4 py-4'>
+              <div className='grid grid-cols-4 space-x-8 items-center justify-center'>
+                <div>
+                  <FormArea
+                    id={'homeTeam'}
+                    label='Home Team'
+                    type='select'
+                    form={form}
+                    name={'homeTeam'}
+                    placeholder={'Select Home Team'}
+                    defaultValue={match?.homeTeam.split(':')[0]}
+                    items={teams}
+                    idFlag={true}
+                  />
+                </div>
+                <div>
+                  <FormArea
+                    id={'awayTeam'}
+                    label='Away Team'
+                    type='select'
+                    form={form}
+                    name={'awayTeam'}
+                    placeholder={'Select Away Team'}
+                    defaultValue={match?.awayTeam.split(':')[0]}
+                    items={teams}
+                    idFlag={true}
+                  />
+                </div>
+                <div>
+                  <FormArea
+                    id={'startDate'}
+                    label='Date'
+                    type='date'
+                    form={form}
+                    name={'startDate'}
+                    time={true}
+                    placeholder={match?.startDate}
+                  />
+                </div>
+                <div className='flex items-center mt-7 p-5'>
+                  <Button type='submit'>Save</Button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ScheduleModal;
