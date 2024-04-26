d1a392da124461474c79b89a7ebd64a5919c4665
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-04-26 19:02:41 +0200

    Add download xlsx file option

--- src/components/home/leagues/TableComponent.jsx
+++ src/components/home/leagues/TableComponent.jsx
@@ -2,9 +2,23 @@ import { Card } from '../../ui/card';
 import CustomDataTable from '../../CustomDataTable';
 import { Link } from 'react-router-dom';
 import { useFetchTeamStatsByLeagueId } from '../../hooks/useFetchTeamStatsByLeagueId';
+import { Button } from '../../ui/button';
 
 const TableComponent = ({ leagueId }) => {
   const { teamStats } = useFetchTeamStatsByLeagueId(leagueId);
+
+  const handleDownloadXLSX = async () => {
+    const res = await fetch(`/api/team/download-xlsx/${leagueId}`);
+    const blob = await res.blob();
+    const url = window.URL.createObjectURL(new Blob([blob]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'LeagueStandings.xlsx');
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode.removeChild(link);
+  };
+
   const columns = [
     {
       name: '#',
@@ -77,9 +91,19 @@ const TableComponent = ({ leagueId }) => {
   ];
 
   return (
-    <Card className='flex flex-col mt-5 w-full rounded-none shadow-md'>
-      <CustomDataTable columns={columns} data={teamStats} pending />
-    </Card>
+    <>
+      <div className='flex max-sm:my-10 justify-end'>
+        <Button
+          className='bg-primary-500 hover:bg-purple-500'
+          onClick={handleDownloadXLSX}
+        >
+          Download Standings.xlsx
+        </Button>
+      </div>
+      <Card className='flex flex-col mt-5 w-full rounded-none shadow-md'>
+        <CustomDataTable columns={columns} data={teamStats} pending />
+      </Card>
+    </>
   );
 };
 
