5fc2c5b3379699613be198b9af87d0f31f021875
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-04-26 19:18:51 +0200

    Handle dates

--- src/components/referee/ScheduleModal.jsx
+++ src/components/referee/ScheduleModal.jsx
@@ -13,27 +13,30 @@ import {
 
 import { useForm } from 'react-hook-form';
 import { Form } from '../ui/form';
+import Spinner from '../Spinner';
 
 const schema = z.object({
   homeTeam: z.string().min(1, { message: 'Home team is required' }),
   awayTeam: z.string().min(1, { message: 'Away team is required' }),
-  startDate: z.coerce
-    .date()
-    .refine(
-      (val) => val.toLocaleDateString() >= new Date().toLocaleDateString(),
-      {
-        message: 'Date must be in the future or today',
-      }
-    ),
+  startDate: z.coerce.date().refine(
+    (val) => {
+      const date = new Date(val);
+      const today = new Date();
+      return date.getTime() <= today.getTime();
+    },
+    {
+      message: 'Date must be in the future or today',
+    }
+  ),
 });
 
-const ScheduleModal = ({ match, teams }) => {
+const ScheduleModal = ({ match, teams, loading }) => {
   const form = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
-      homeTeam: match?.homeTeam || '',
-      awayTeam: match?.awayTeam || '',
-      startDate: match?.startDate || '',
+      homeTeam: match?.homeTeam,
+      awayTeam: match?.awayTeam,
+      startDate: match?.startDate,
     },
     mode: 'onChange',
   });
@@ -41,13 +44,13 @@ const ScheduleModal = ({ match, teams }) => {
   const onSubmit = async (data) => {
     const updatedData = {
       ...data,
-      homeTeam: data.homeTeam.split(':')[1]
-        ? data.homeTeam.split(':')[1]
-        : data.homeTeam,
-      awayTeam: data.awayTeam.split(':')[1]
-        ? data.awayTeam.split(':')[1]
-        : data.awayTeam,
-      startDate: new Date(data.startDate).toISOString(),
+      homeTeam: data?.homeTeam?.split(':')[1]
+        ? data?.homeTeam?.split(':')[1]
+        : data?.homeTeam,
+      awayTeam: data?.awayTeam?.split(':')[1]
+        ? data?.awayTeam?.split(':')[1]
+        : data?.awayTeam,
+      startDate: new Date(data?.startDate).toISOString(),
     };
 
     try {
@@ -66,6 +69,14 @@ const ScheduleModal = ({ match, teams }) => {
       console.error(error);
     }
   };
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <Dialog>
@@ -88,12 +99,12 @@ const ScheduleModal = ({ match, teams }) => {
               <div className='grid grid-cols-4 space-x-8 items-center justify-center'>
                 <div>
                   <FormArea
-                    id={'homeTeam'}
+                    id='homeTeam'
                     label='Home Team'
                     type='select'
                     form={form}
-                    name={'homeTeam'}
-                    placeholder={'Select Home Team'}
+                    name='homeTeam'
+                    placeholder='Select Home Team'
                     defaultValue={match?.homeTeam.split(':')[0]}
                     items={teams}
                     idFlag={true}
@@ -101,12 +112,12 @@ const ScheduleModal = ({ match, teams }) => {
                 </div>
                 <div>
                   <FormArea
-                    id={'awayTeam'}
+                    id='awayTeam'
                     label='Away Team'
                     type='select'
                     form={form}
-                    name={'awayTeam'}
-                    placeholder={'Select Away Team'}
+                    name='awayTeam'
+                    placeholder='Select Away Team'
                     defaultValue={match?.awayTeam.split(':')[0]}
                     items={teams}
                     idFlag={true}
@@ -114,17 +125,24 @@ const ScheduleModal = ({ match, teams }) => {
                 </div>
                 <div>
                   <FormArea
-                    id={'startDate'}
+                    id='startDate'
                     label='Date'
                     type='date'
                     form={form}
-                    name={'startDate'}
-                    time={true}
-                    placeholder={match?.startDate}
+                    name='startDate'
+                    placeholder='Select date'
+                    initialDate={match?.startDate}
+                    isPortal={false}
+                    time
                   />
                 </div>
                 <div className='flex items-center mt-7 p-5'>
-                  <Button type='submit'>Save</Button>
+                  <Button
+                    type='submit'
+                    className='bg-primary-500 hover:bg-purple-500'
+                  >
+                    Save
+                  </Button>
                 </div>
               </div>
             </div>
