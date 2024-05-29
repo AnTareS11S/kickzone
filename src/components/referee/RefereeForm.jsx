677684437419121df8099beb998c5d8a8b24ac2b
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:13:12 +0200

    Refactor RefereeForm component

--- src/components/referee/RefereeForm.jsx
+++ src/components/referee/RefereeForm.jsx
@@ -4,19 +4,19 @@ import { Form } from '../ui/form';
 import FormArea from '../FormArea';
 import { Button } from '../ui/button';
 import { useEffect, useRef, useState } from 'react';
-import uploadFile from '../../lib/uploadFile';
 import { usersFormSchema } from '../../lib/validation/UsersValidation';
 import { useFetchCountries } from '../hooks/useFetchCountries';
 import { useToast } from '../ui/use-toast';
 import { useFetchRefereeByUserId } from '../hooks/useFetchReferee';
 import Spinner from '../Spinner';
+import { Card } from '../ui/card';
 
 const RefereeForm = ({ currentUser }) => {
   const fileRef = useRef(null);
-  const [file, setFile] = useState(undefined);
-  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [file, setFile] = useState();
   const countries = useFetchCountries();
-  const { referee: refereeData, loading } = useFetchRefereeByUserId();
+  const [isChanged, setIsChanged] = useState(false);
+  const { referee: refereeData, loading } = useFetchRefereeByUserId(isChanged);
   const { toast } = useToast();
 
   const form = useForm({
@@ -42,31 +42,10 @@ const RefereeForm = ({ currentUser }) => {
         city: refereeData?.city || '',
         bio: refereeData?.bio || '',
         birthDate: refereeData?.birthDate || '',
-        photo: refereeData?.photo || '',
+        photo: refereeData?.imageUrl || '',
       });
     }
-  }, [refereeData, form]);
-
-  useEffect(() => {
-    if (file) {
-      handleFileUpload(file);
-    }
-    const timeoutId = setTimeout(() => {
-      setUpdateSuccess(false);
-    }, 3000);
-
-    return () => clearTimeout(timeoutId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [updateSuccess, file]);
-
-  const handleFileUpload = async (file) => {
-    try {
-      const downloadURL = await uploadFile(file);
-      form.setValue('photo', downloadURL);
-    } catch (error) {
-      console.log('Error uploading file: ', error);
-    }
-  };
+  }, [refereeData, form, isChanged]);
 
   const originalDate = form.getValues('birthDate');
   const formattedDate = new Date(originalDate);
@@ -80,20 +59,24 @@ const RefereeForm = ({ currentUser }) => {
       country.split(':')[0].includes(formData?.nationality)
     );
 
-    const updatedData = {
-      ...formData,
-      user: currentUser?._id,
-      photo: form.getValues('photo'),
-      nationality: countryId?.split(':')[1],
-      dateOfBirth: formattedDate,
-    };
+    const data = new FormData();
+
+    data.append('photo', file || refereeData?.photo);
+    data.append('name', formData.name);
+    data.append('surname', formData.surname);
+    data.append('bio', formData.bio);
+    data.append('city', formData.city);
+    data.append('birthDate', formattedDate);
+    data.append(
+      'nationality',
+      countryId?.split(':')[1] || formData.nationality
+    );
+    data.append('user', currentUser?._id);
+
     try {
-      const res = await fetch('/api/referee/create', {
+      const res = await fetch('/api/referee/add', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedData),
+        body: data,
       });
 
       if (res.ok) {
@@ -101,6 +84,7 @@ const RefereeForm = ({ currentUser }) => {
           title: 'Success!',
           description: 'Referee profile updated successfully',
         });
+        setIsChanged(!isChanged);
       } else {
         toast({
           title: 'Error!',
@@ -108,73 +92,95 @@ const RefereeForm = ({ currentUser }) => {
           variant: 'destructive',
         });
       }
-      setUpdateSuccess(true);
     } catch (error) {
       console.log(error);
     }
   };
 
   if (loading) {
-    return (
-      <div className='flex items-center justify-center h-full'>
-        <Spinner />
-      </div>
-    );
+    return <Spinner />;
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className='flex flex-col space-y-4'
-      >
-        <FormArea id='name' label='Name' type='text' form={form} name='name' />
-        <FormArea
-          id='surname'
-          label='Surname'
-          type='text'
-          form={form}
-          name='surname'
-        />
-        <FormArea
-          id='birthDate'
-          label='Date of Birth'
-          type='date'
-          form={form}
-          name='birthDate'
-          isEdit={true}
-          initialDate={refereeData?.birthDate}
-          placeholder='Select date'
-        />
-        <FormArea
-          id='photo'
-          label='Photo'
-          type='file'
-          form={form}
-          name='photo'
-          fileRef={fileRef}
-          setFile={setFile}
-          currentUserPhoto={refereeData?.photo}
-        />
-        <FormArea id='bio' label='Bio' type='textarea' form={form} name='bio' />
-        <FormArea
-          id='nationality'
-          type='select'
-          form={form}
-          items={countries}
-          label='Nationality'
-          placeholder={countryName?.split(':')[0] || 'Select nationality'}
-          name='nationality'
-        />
-        <FormArea id='city' label='City' type='text' form={form} name='city' />
+    <Card className='px-4 py-8'>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          encType='multipart/form-data'
+        >
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+            <FormArea
+              id='name'
+              label='Name'
+              type='text'
+              form={form}
+              name='name'
+            />
+            <FormArea
+              id='surname'
+              label='Surname'
+              type='text'
+              form={form}
+              name='surname'
+            />
+          </div>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+            <FormArea
+              id='birthDate'
+              label='Date of Birth'
+              type='date'
+              form={form}
+              name='birthDate'
+              isEdit={true}
+              initialDate={refereeData?.birthDate || new Date()}
+              placeholder='Select date'
+            />
+            <FormArea
+              id='nationality'
+              type='select'
+              form={form}
+              items={countries}
+              label='Nationality'
+              placeholder={countryName?.split(':')[0] || 'Select nationality'}
+              name='nationality'
+            />
+          </div>
+          <FormArea
+            id='bio'
+            label='Bio'
+            type='textarea'
+            form={form}
+            name='bio'
+          />
+          <FormArea
+            id='city'
+            label='City'
+            type='text'
+            form={form}
+            name='city'
+          />
+          <FormArea
+            id='photo'
+            label='Photo'
+            type='file'
+            form={form}
+            name='photo'
+            fileRef={fileRef}
+            setFile={setFile}
+            currentUserPhoto={refereeData?.imageUrl}
+          />
 
-        <div className='flex justify-start items-center'>
-          <Button type='submit' className='bg-primary-500 hover:bg-purple-500'>
-            Save
-          </Button>
-        </div>
-      </form>
-    </Form>
+          <div className='flex justify-end'>
+            <Button
+              type='submit'
+              className='bg-primary-500 hover:bg-purple-500 px-6 py-3 rounded-md text-white font-semibold'
+            >
+              Save
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </Card>
   );
 };
 
