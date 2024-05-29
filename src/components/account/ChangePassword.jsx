97e0b70e5db41f1433a976caf9c085cf6b474513
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:07:34 +0200

    Refactor ChangePassword component to use Card and FormArea components

--- src/components/account/ChangePassword.jsx
+++ src/components/account/ChangePassword.jsx
@@ -1,42 +1,26 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '../ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '../ui/form';
-import { Input } from '../ui/input';
+import { Form } from '../ui/form';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
-
 import { z } from 'zod';
 import { useToast } from '../ui/use-toast';
+import FormArea from '../FormArea';
+import { Card } from '../ui/card';
 
 const passwordSchema = z
   .object({
-    currentPassword: z.string().min(6, {
-      message: 'Password must be at least 6 characters.',
-    }),
+    currentPassword: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters.' }),
     newPassword: z
       .string()
-      .min(6, {
-        message: 'Password must be at least 6 characters.',
-      })
-      .max(20, {
-        message: 'Password must not be longer than 20 characters.',
-      }),
-
+      .min(6, { message: 'Password must be at least 6 characters.' })
+      .max(20, { message: 'Password must not be longer than 20 characters.' }),
     confirmPassword: z
       .string()
-      .min(6, {
-        message: 'Password must be at least 6 characters.',
-      })
-      .max(20, {
-        message: 'Password must not be longer than 20 characters.',
-      }),
+      .min(6, { message: 'Password must be at least 6 characters.' })
+      .max(20, { message: 'Password must not be longer than 20 characters.' }),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: 'Passwords do not match.',
@@ -71,7 +55,6 @@ const ChangePassword = () => {
           type: 'manual',
           message: 'Invalid password',
         });
-        return;
       }
       if (res.status === 200) {
         toast({
@@ -92,73 +75,40 @@ const ChangePassword = () => {
   };
 
   return (
-    <Form {...form}>
-      <form className='space-y-6' onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name='currentPassword'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Current Password</FormLabel>
-              <FormControl>
-                <Input
-                  type='password'
-                  id='currentPassword'
-                  className='w-full'
-                  {...field}
-                  {...form.register('currentPassword')}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='newPassword'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>New Password</FormLabel>
-              <FormControl>
-                <Input
-                  type='password'
-                  id='newPassword'
-                  className='w-full'
-                  {...field}
-                  {...form.register('newPassword')}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='confirmPassword'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input
-                  type='password'
-                  id='confirmPassword'
-                  className='w-full'
-                  {...field}
-                  {...form.register('confirmPassword')}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          type='submit'
-          className='btn bg-primary-500 hover:bg-purple-500'
-        >
-          Save
-        </Button>
-      </form>
-    </Form>
+    <Card className='px-4 py-8'>
+      <h2 className='text-2xl font-bold mb-6 text-center'>Change Password</h2>
+      <Form {...form}>
+        <form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
+          <FormArea
+            id='currentPassword'
+            label='Current Password'
+            type='password'
+            form={form}
+            name='currentPassword'
+          />
+          <FormArea
+            id='newPassword'
+            label='New Password'
+            type='password'
+            form={form}
+            name='newPassword'
+          />
+          <FormArea
+            id='confirmPassword'
+            label='Confirm Password'
+            type='password'
+            form={form}
+            name='confirmPassword'
+          />
+          <Button
+            type='submit'
+            className='w-full bg-primary-500 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded'
+          >
+            Save
+          </Button>
+        </form>
+      </Form>
+    </Card>
   );
 };
 
