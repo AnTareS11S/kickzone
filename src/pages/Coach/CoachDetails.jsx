2f0e344a88d1528c222e9c24e1a99fa816d852f1
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:19:36 +0200

    Refactor CoachDetails component to use custom hook and add new UI elements

--- src/pages/Coach/CoachDetails.jsx
+++ src/pages/Coach/CoachDetails.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
@@ -7,83 +6,74 @@ import {
   CardTitle,
 } from '../../components/ui/card';
 import Spinner from '../../components/Spinner';
+import { useParams } from 'react-router-dom';
+import { useFetchCoachById } from '../../components/hooks/useFetchCoachById';
+import BackButton from '../../components/BackButton';
+import { Separator } from '../../components/ui/separator';
+import { FaFlag, FaCity, FaBirthdayCake } from 'react-icons/fa';
 
 const CoachDetails = () => {
-  const [coach, setCoach] = useState({});
-  const [loading, setLoading] = useState(false);
-  const pathname = window.location.pathname.split('/').pop();
+  const coachId = useParams().id;
+  const { coach, loading } = useFetchCoachById(coachId);
 
-  useEffect(() => {
-    const getCoach = async () => {
-      try {
-        setLoading(true);
-        const res = await fetch(`/api/coach/${pathname}`);
-        const data = await res.json();
-        setCoach(data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getCoach();
-  }, [pathname]);
-
-  if (loading)
-    return (
-      <div className='flex items-center justify-center h-full'>
-        <Spinner />
-      </div>
-    );
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
-    <Card className='bg-white shadow-lg p-6 rounded-md'>
-      <CardHeader className='mb-4 flex items-center flex-row'>
-        <div className='w-40 h-40'>
-          <img
-            src={
-              coach.photo ||
-              'https://firebasestorage.googleapis.com/v0/b/futbolistapro.appspot.com/o/avatars%2Fblank-profile-picture-973460_960_720.webp?alt=media&token=5779eb88-d84b-46f3-bef6-3c2648a8fc9c'
-            }
-            alt={`Photo of ${coach.name}`}
-            className='object-contain h-full w-full rounded-full mt-4'
-          />
-        </div>
-        <div className='ml-4 flex flex-col justify-between'>
-          <CardTitle className='text-heading2-semibold mb-2'>
-            {coach.name + ' ' + coach.surname}
-          </CardTitle>
-          <CardDescription className='text-gray-700'>
-            {coach.bio}
-          </CardDescription>
-        </div>
-      </CardHeader>
-      <CardContent className='grid grid-cols-2 gap-6 pl-12'>
-        <div>
-          <p className='text-sm font-semibold text-gray-500'>Country:</p>
-          <p className='text-gray-800'>{coach.nationality}</p>
-        </div>
-        <div>
-          <p className='text-sm font-semibold text-gray-500'>City:</p>
-          <p className='text-gray-800'>{coach.city}</p>
-        </div>
-        <div>
-          <p className='text-sm font-semibold text-gray-500'>Birth Date:</p>
-          <p className='text-gray-800'>
-            {coach.birthDate?.toString().slice(0, 10)}
-          </p>
-        </div>
-        <div>
-          <p className='text-sm font-semibold text-gray-500'>Teams:</p>
-          <ul className='list-disc pl-4 text-gray-800'>
-            {coach.teams?.map((team, index) => (
-              <li key={index}>{team.split(':')[0]}</li>
-            ))}
-          </ul>
-        </div>
-      </CardContent>
-    </Card>
+    <article className='py-8'>
+      <BackButton />
+      <Separator />
+      <Card className='bg-white shadow-lg rounded-lg flex flex-col'>
+        <CardHeader className='bg-gray-100 p-6  md:flex-row items-center'>
+          <div className='w-40 h-40 rounded-full  mb-4 md:mb-0 md:mr-6'>
+            <img
+              src={
+                coach?.imageUrl ||
+                'https://d3awt09vrts30h.cloudfront.net/blank-profile-picture.webp'
+              }
+              alt={`Photo of ${coach?.name}`}
+              className='object-cover w-full h-full'
+            />
+          </div>
+          <div>
+            <CardTitle className='text-2xl font-bold mb-2'>
+              {coach?.name + ' ' + coach?.surname}
+            </CardTitle>
+            <CardDescription className='text-gray-600'>
+              {coach?.bio} | {coach?.currentTeam}
+            </CardDescription>
+          </div>
+        </CardHeader>
+        <CardContent className='p-6 grid grid-cols-1 md:grid-cols-2 gap-8'>
+          <div>
+            <h3 className='text-lg font-semibold mb-2'>Personal Information</h3>
+            <div className='flex items-center mb-2'>
+              <FaBirthdayCake className='text-gray-500 mr-2' />
+              <p className='text-gray-700'>
+                {coach.birthDate?.toString().slice(0, 10)}
+              </p>
+            </div>
+            <div className='flex items-center mb-2'>
+              <FaFlag className='text-gray-500 mr-2' />
+              <p className='text-gray-700'>{coach?.nationality}</p>
+            </div>
+            <div className='flex items-center mb-2'>
+              <FaCity className='text-gray-500 mr-2' />
+              <p className='text-gray-700'>{coach?.city}</p>
+            </div>
+          </div>
+          <div>
+            <h3 className='text-lg font-semibold mb-2'>Previous Teams</h3>
+            <ul className='list-disc pl-4 text-gray-700'>
+              {coach.teams?.map((team, index) => (
+                <li key={index}>{team.split(':')[0]}</li>
+              ))}
+            </ul>
+          </div>
+        </CardContent>
+      </Card>
+    </article>
   );
 };
 
