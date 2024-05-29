bae8bf114e4f0d6ce29b716907565f1a88cba760
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:09:37 +0200

    Refactor TeamResult component to use custom hook and add loading spinner

--- src/components/home/team/TeamResult.jsx
+++ src/components/home/team/TeamResult.jsx
@@ -1,65 +1,74 @@
-import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Card, CardContent } from '../../ui/card';
+import { useFetchResultsAndMatchesByTeamId } from '../../hooks/useFetchResultsAndMatchesByTeamId';
+import Spinner from '../../Spinner';
 
 const TeamResult = () => {
   const teamId = useLocation().pathname.split('/').pop();
-  const [results, setResults] = useState([]);
-  const [matches, setMatches] = useState([]);
+  const { results, matches, loading } =
+    useFetchResultsAndMatchesByTeamId(teamId);
 
-  useEffect(() => {
-    const getResults = async () => {
-      try {
-        const res = await fetch(`/api/team/results/${teamId}`);
-        const data = await res.json();
-        setResults(data.results);
-        setMatches(data.matches);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getResults();
-  }, [teamId]);
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
-    <Card className='mb-5 mt-5'>
-      <CardContent className='p-6'>
-        {matches?.map((match) => (
-          <Link to={`/results/${match?._id}`} key={match?._id}>
-            <div className='border cursor-pointer p-4 rounded-md hover:shadow-md transition-all mb-4 hover:bg-gray-200 '>
-              <div className='flex items-center justify-between p-2 rounded-md transition-all'>
-                <div className='flex items-center space-x-4'>
-                  {results?.map((result) => (
-                    <p
-                      className='text-sm text-gray-800 font-semibold'
-                      key={result?._id}
-                    >
-                      {match?.homeTeam?.name} {result?.homeTeamScore} :{' '}
-                      {result?.awayTeamScore} {match?.awayTeam?.name}
-                    </p>
+    <>
+      {matches?.length > 0 ? (
+        <Card className='mb-5 mt-5'>
+          <CardContent className='p-6'>
+            {matches?.map((match) => (
+              <div
+                key={match?._id}
+                className='border cursor-pointer p-4 rounded-md hover:shadow-md transition-all mb-4 hover:bg-gray-200 '
+              >
+                {results
+                  .filter((result) => result.match === match._id)
+                  .map((result) => (
+                    <Link to={`/results/${result?._id}`} key={result?._id}>
+                      <div className='flex items-center justify-between p-2 rounded-md transition-all'>
+                        <div className='flex items-center space-x-4'>
+                          <p
+                            className='text-sm text-gray-800 font-semibold'
+                            key={result?._id}
+                          >
+                            {match?.homeTeam?.name} {result?.homeTeamScore} :{' '}
+                            {result?.awayTeamScore} {match?.awayTeam?.name}
+                          </p>
+                        </div>
+                        <div className='flex items-center space-x-4'>
+                          <p className='text-sm text-gray-500'>
+                            {new Date(match?.startDate).toLocaleDateString(
+                              'en-GB',
+                              {
+                                day: 'numeric',
+                                month: 'long',
+                              }
+                            )}
+                          </p>
+                          <p className='text-sm text-gray-500'>
+                            {new Date(match?.startDate).toLocaleTimeString(
+                              'en-GB',
+                              {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                              }
+                            )}
+                          </p>
+                        </div>
+                      </div>
+                    </Link>
                   ))}
-                </div>
-                <div className='flex items-center space-x-4'>
-                  <p className='text-sm text-gray-500'>
-                    {new Date(match?.startDate).toLocaleDateString('en-GB', {
-                      day: 'numeric',
-                      month: 'long',
-                    })}
-                  </p>
-                  <p className='text-sm text-gray-500'>
-                    {new Date(match?.startDate).toLocaleTimeString('en-GB', {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </p>
-                </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </CardContent>
-    </Card>
+            ))}
+          </CardContent>
+        </Card>
+      ) : (
+        <div className='flex items-center justify-center h-full'>
+          <p className='text-gray-600'>No results found.</p>
+        </div>
+      )}
+    </>
   );
 };
 
