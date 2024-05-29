41847ea87a9721f7b8b97a820aff98ba13e770aa
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:18:59 +0200

    Refactor code

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,14 +3,21 @@ import { Input } from '../components/ui/input';
 import { Link } from 'react-router-dom';
 import { Card } from '../components/ui/card';
 import { Separator } from '../components/ui/separator';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+import { Button } from '../components/ui/button';
 
 const Search = () => {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState(
+    localStorage.getItem('lastSearch') || ''
+  );
+
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const handleSearch = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch(`/api/player?q=${search}`);
         if (res.ok) {
           const data = await res.json();
@@ -18,93 +25,199 @@ const Search = () => {
         }
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
-    handleSearch();
+    if (search) {
+      handleSearch();
+    } else {
+      setSearchResults({});
+    }
+
+    // Save the current search value to localStorage
+    localStorage.setItem('lastSearch', search);
   }, [search]);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
 
+  const clearSearch = () => {
+    setSearch('');
+  };
+
   return (
-    <>
-      <div className='searchbar mt-5 relative'>
-        <img
-          src='/public/search.svg'
-          alt='search'
-          width={25}
-          height={25}
-          className='object-contain absolute top-1/2 transform -translate-y-1/2 left-5'
-        />
+    <div className='container mx-auto p-4'>
+      <div className='flex items-center bg-white rounded-md shadow-sm'>
+        <FaSearch className='text-gray-500 ml-4' />
         <Input
           id='text'
           value={search}
           onChange={handleChange}
           autoComplete='off'
           placeholder='Search...'
-          className='w-full ring-2 bg-slate-200 text-text-text-heading2-semibold ring-white hover:ring-primary-500 transition duration-300 p-6 pl-9'
+          className='flex-grow py-2 px-4 border-none bg-transparent focus-visible:outline-none focus-visible:ring-0'
         />
+        {search && (
+          <Button
+            onClick={clearSearch}
+            className='focus:outline-none shadow-none bg-0 hover:bg-0'
+          >
+            <FaTimes className='text-gray-500 mx-2 cursor-pointer' />
+          </Button>
+        )}
       </div>
-      {search && searchResults?.players?.length !== 0 && (
-        <div className='m-4'>
-          <p className='text-heading3-bold '>Players</p>
-          <Separator className='my-4' />
-        </div>
-      )}
-      {search &&
-        searchResults?.players?.map((player) => (
-          <Card key={player._id} className='user-card m-4 flex items-center'>
-            <img
-              src={player.photo}
-              alt={player.name}
-              className='rounded-full object-cover mr-2 flex-shrink-0 w-16 h-16'
-            />
-            <Link
-              to={`/player/${player._id}`}
-              className='flex-1 text-ellipsis text-base-semibold'
-            >
-              {player.name} {player.surname}
-            </Link>
-          </Card>
-        ))}
-      {search && searchResults?.teams?.length !== 0 && (
-        <div className='m-4'>
-          <p className='text-heading3-bold'>Teams</p>
-          <Separator className='my-4' />
-        </div>
-      )}
-      {search &&
-        searchResults?.teams?.map((team) => (
-          <Card key={team._id} className='user-card  m-4 flex items-center'>
-            <img
-              src={team.logo}
-              alt={team.name}
-              className='rounded-full object-cover mr-2 flex-shrink-0 w-16 h-16'
-            />
-            <Link
-              to={`/league/team/${team._id}`}
-              className='flex-1 text-ellipsis text-base-semibold'
-            >
-              {team.name}
-            </Link>
-          </Card>
-        ))}
 
-      {!search && (
-        <div className='w-fit mx-auto my-40'>
-          <p className='text-body-semibold'>
-            Search for players and teams to explore.
+      {isLoading && (
+        <div className='my-8 text-center'>
+          <p className='text-gray-500'>Loading...</p>
+        </div>
+      )}
+
+      {!isLoading && search && searchResults?.players?.length > 0 && (
+        <div className='my-8'>
+          <h3 className='text-lg font-bold mb-4'>Players</h3>
+          <Separator className='mb-4' />
+          {searchResults.players.map((player) => (
+            <Card
+              key={player._id}
+              className='bg-white rounded-md shadow-sm mb-4 hover:shadow-md transition-shadow duration-300'
+            >
+              <Link
+                to={`/player/${player._id}`}
+                className='flex items-center p-4'
+              >
+                <img
+                  src={
+                    player.imageUrl ||
+                    'https://d3awt09vrts30h.cloudfront.net/blank-profile-picture.webp'
+                  }
+                  alt={player.name}
+                  className='rounded-full w-12 h-12 object-cover mr-4'
+                />
+                <div className='flex-grow'>
+                  <h4 className='text-base font-semibold'>
+                    {player.name} {player.surname}
+                  </h4>
+                </div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+      )}
+
+      {!isLoading && search && searchResults?.coaches?.length > 0 && (
+        <div className='my-8'>
+          <h3 className='text-lg font-bold mb-4'>Coches</h3>
+          <Separator className='mb-4' />
+          {searchResults.coaches?.map((coach) => (
+            <Card
+              key={coach._id}
+              className='bg-white rounded-md shadow-sm mb-4 hover:shadow-md transition-shadow duration-300'
+            >
+              <Link
+                to={`/coach/${coach._id}`}
+                className='flex items-center p-4'
+              >
+                <img
+                  src={
+                    coach.imageUrl ||
+                    'https://d3awt09vrts30h.cloudfront.net/blank-profile-picture.webp'
+                  }
+                  alt={coach.name}
+                  className='rounded-full w-12 h-12 object-cover mr-4'
+                />
+                <div className='flex-grow'>
+                  <h4 className='text-base font-semibold'>
+                    {coach.name} {coach.surname}
+                  </h4>
+                </div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+      )}
+      {!isLoading && search && searchResults?.referees?.length > 0 && (
+        <div className='my-8'>
+          <h3 className='text-lg font-bold mb-4'>Referees</h3>
+          <Separator className='mb-4' />
+          {searchResults.referees?.map((referee) => (
+            <Card
+              key={referee._id}
+              className='bg-white rounded-md shadow-sm mb-4 hover:shadow-md transition-shadow duration-300'
+            >
+              <Link
+                to={`/referee/${referee._id}`}
+                className='flex items-center p-4'
+              >
+                <img
+                  src={
+                    referee.imageUrl ||
+                    'https://d3awt09vrts30h.cloudfront.net/blank-profile-picture.webp'
+                  }
+                  alt={referee.name}
+                  className='rounded-full w-12 h-12 object-cover mr-4'
+                />
+                <div className='flex-grow'>
+                  <h4 className='text-base font-semibold'>
+                    {referee.name} {referee.surname}
+                  </h4>
+                </div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+      )}
+      {!isLoading && search && searchResults?.teams?.length > 0 && (
+        <div className='my-8'>
+          <h3 className='text-lg font-bold mb-4'>Teams</h3>
+          <Separator className='mb-4' />
+          {searchResults.teams?.map((team) => (
+            <Card
+              key={team._id}
+              className='bg-white rounded-md shadow-sm mb-4 hover:shadow-md transition-shadow duration-300'
+            >
+              <Link
+                to={`/league/team/${team._id}`}
+                className='flex items-center p-4'
+              >
+                <img
+                  src={
+                    team.logoUrl ||
+                    'https://d3awt09vrts30h.cloudfront.net/team_img_default.png'
+                  }
+                  alt={team.name}
+                  className='rounded-full w-12 h-12 object-cover mr-4'
+                />
+                <div className='flex-grow'>
+                  <h4 className='text-base font-semibold'>{team.name}</h4>
+                </div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+      )}
+
+      {!isLoading &&
+        search &&
+        !searchResults.players?.length &&
+        !searchResults.coaches?.length &&
+        !searchResults.referees?.length &&
+        !searchResults.teams?.length && (
+          <div className='my-8 text-center'>
+            <p className='text-gray-500'>No results found.</p>
+          </div>
+        )}
+
+      {!isLoading && !search && (
+        <div className='my-8 text-center'>
+          <p className='text-gray-500'>
+            Search for players, coaches, referees, and teams.
           </p>
         </div>
       )}
-
-      {search && searchResults?.players?.length === 0 && (
-        <div className='w-fit mx-auto my-40'>
-          <p className='text-body-semibold'>No results found.</p>
-        </div>
-      )}
-    </>
+    </div>
   );
 };
 
