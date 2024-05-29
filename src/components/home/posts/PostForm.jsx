f751745568a8f5bf936451800a3c3d032e68b9e4
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:08:34 +0200

    Refactor PostForm component and add Card component

--- src/components/home/posts/PostForm.jsx
+++ src/components/home/posts/PostForm.jsx
@@ -2,66 +2,44 @@ import { useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { Form } from '../../ui/form';
 import FormArea from '../../FormArea';
-import { useEffect, useRef, useState } from 'react';
-import uploadFile from '../../../lib/uploadFile';
+import { useRef, useState } from 'react';
 import { Button } from '../../ui/button';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { PostValidation } from '../../../lib/validation/PostValidation';
+import { postFormSchema } from '../../../lib/validation/PostValidation';
 import { useNavigate } from 'react-router-dom';
+import { Card, CardHeader, CardContent } from '../../ui/card';
+import { FaEdit } from 'react-icons/fa';
 
 const PostForm = () => {
   const fileRef = useRef(null);
-  const [file, setFile] = useState(undefined);
-  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [file, setFile] = useState();
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
-
   const form = useForm({
-    resolver: zodResolver(PostValidation),
+    resolver: zodResolver(postFormSchema(false)),
     defaultValues: {
       title: '',
-      content: '',
-      photo: '',
+      postContent: '',
+      postPhoto: '',
     },
     mode: 'onChange',
   });
 
-  useEffect(() => {
-    if (file) {
-      handleFileUpload(file);
-    }
-    const timeoutId = setTimeout(() => {
-      setUpdateSuccess(false);
-    }, 3000);
-
-    return () => clearTimeout(timeoutId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [updateSuccess, file]);
-
-  const handleFileUpload = async (file) => {
-    try {
-      const downloadURL = await uploadFile(file);
-      form.setValue('photo', downloadURL);
-    } catch (error) {
-      console.log('Error uploading file: ', error);
-    }
-  };
-
   const onSubmit = async (formData) => {
-    const updatedData = { ...formData, author: currentUser._id };
+    const data = new FormData();
+    data.append('title', formData.title);
+    data.append('postContent', formData.postContent);
+    data.append('postPhoto', file);
+    data.append('author', currentUser._id);
     try {
       const res = await fetch('/api/post/add', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedData),
+        body: data,
       });
       if (!res.ok) {
         throw new Error('Failed to fetch data!');
       }
       form.reset();
-      setUpdateSuccess(true);
       navigate('/');
     } catch (error) {
       console.log('Error creating post: ', error);
@@ -69,44 +47,60 @@ const PostForm = () => {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormArea
-          id='title'
-          label='Title'
-          type='text'
-          form={form}
-          name='title'
-        />
-        <FormArea
-          id='content'
-          label='Content'
-          type='textarea'
-          form={form}
-          name='postContent'
-        />
-        <FormArea
-          id='photo'
-          label='Photo'
-          type='file'
-          form={form}
-          name='postPhoto'
-          fileRef={fileRef}
-          setFile={setFile}
-        />
-
-        <div className='flex justify-start items-center mt-5'>
-          <Button
-            type='submit'
-            disabled={!form.formState.isValid}
-            className='bg-primary-500 text-white'
-          >
-            Save
-          </Button>
-          {updateSuccess && <p className='text-green-700 ml-3'>Saved</p>}
-        </div>
-      </form>
-    </Form>
+    <div className='container mx-auto py-8'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card className='bg-white shadow-md rounded-lg'>
+            <CardHeader className='bg-gray-100 p-4 rounded-t-lg flex items-center'>
+              <FaEdit className='text-gray-500 mr-2' />
+              <h2 className='text-lg font-semibold'>Create Post</h2>
+            </CardHeader>
+            <CardContent className='p-6'>
+              <div className='mb-6'>
+                <FormArea
+                  id='title'
+                  label='Title'
+                  type='text'
+                  form={form}
+                  name='title'
+                  className='w-full'
+                />
+              </div>
+              <div className='mb-6'>
+                <FormArea
+                  id='content'
+                  label='Content'
+                  type='textarea'
+                  form={form}
+                  name='postContent'
+                  className='w-full'
+                />
+              </div>
+              <div className='mb-6 w-fit'>
+                <FormArea
+                  id='postPhoto'
+                  label='Photo'
+                  type='file'
+                  form={form}
+                  name='postPhoto'
+                  fileRef={fileRef}
+                  setFile={setFile}
+                  className='w-full'
+                />
+              </div>
+              <div className='flex justify-end'>
+                <Button
+                  type='submit'
+                  className='bg-primary-500 hover:bg-primary-600 text-white py-2 px-4 rounded-md shadow transition-colors duration-300'
+                >
+                  Save
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </div>
   );
 };
 
