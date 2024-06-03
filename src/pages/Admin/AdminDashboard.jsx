30714df1ee361386a0efa177a1e56cc79abc080b
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-03 18:17:21 +0200

    Add new cards for players, coaches, referees, and sponsors in AdminDashboard.jsx

--- src/pages/Admin/AdminDashboard.jsx
+++ src/pages/Admin/AdminDashboard.jsx
@@ -11,15 +11,16 @@ const AdminDashboard = () => {
 
       <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
         <AdminCard title='Users' linkTo='/dashboard/admin/users' />
+        <AdminCard title='Players' linkTo='/dashboard/admin/players' />
+        <AdminCard title='Coaches' linkTo='/dashboard/admin/coaches' />
+        <AdminCard title='Referees' linkTo='/dashboard/admin/referees' />
+        <AdminCard title='Sponsors' linkTo='/dashboard/admin/sponsors' />
         <AdminCard title='Teams' linkTo='/dashboard/admin/teams' />
         <AdminCard title='Leagues' linkTo='/dashboard/admin/leagues' />
-        <AdminCard title='Players' linkTo='/dashboard/admin/players' />
-        <AdminCard title='Countries' linkTo='/dashboard/admin/countries' />
         <AdminCard title='Stadiums' linkTo='/dashboard/admin/stadiums' />
         <AdminCard title='Positions' linkTo='/dashboard/admin/positions' />
-        <AdminCard title='Coaches' linkTo='/dashboard/admin/coaches' />
+        <AdminCard title='Countries' linkTo='/dashboard/admin/countries' />
         <AdminCard title='Seasons' linkTo='/dashboard/admin/seasons' />
-        <AdminCard title='Sponsors' linkTo='/dashboard/admin/sponsors' />
       </div>
     </div>
   );
