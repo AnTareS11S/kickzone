c3c731766a3accc45b20fc6f3c955b42679fb6fc
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-03 18:17:49 +0200

    Refactor usersFormSchema in UsersValidation.js

--- src/lib/validation/UsersValidation.js
+++ src/lib/validation/UsersValidation.js
@@ -1,44 +1,45 @@
 import * as z from 'zod';
-export const usersFormSchema = z.object({
-  name: z
-    .string()
-    .min(1, {
-      message: 'Name must be at least 4 characters.',
-    })
-    .max(30, {
-      message: 'Name must not be longer than 30 characters.',
+export const usersFormSchema = () =>
+  z.object({
+    name: z
+      .string()
+      .min(1, {
+        message: 'Name must be at least 4 characters.',
+      })
+      .max(30, {
+        message: 'Name must not be longer than 30 characters.',
+      }),
+    surname: z
+      .string()
+      .min(1, {
+        message: 'Surname must be at least 4 characters.',
+      })
+      .max(30, {
+        message: 'Surname must not be longer than 30 characters.',
+      }),
+    birthDate: z.coerce.date().refine(
+      (val) => {
+        const date = new Date(val);
+        const today = new Date();
+        return date.getTime() <= today.getTime();
+      },
+      {
+        message: 'Birth date must be in the past or today',
+      }
+    ),
+    nationality: z.string().min(1, {
+      message: 'Nationality is required',
     }),
-  surname: z
-    .string()
-    .min(1, {
-      message: 'Surname must be at least 4 characters.',
-    })
-    .max(30, {
-      message: 'Surname must not be longer than 30 characters.',
+    city: z.string().min(1, {
+      message: 'City is required',
     }),
-  birthDate: z.coerce.date().refine(
-    (val) => {
-      const date = new Date(val);
-      const today = new Date();
-      return date.getTime() <= today.getTime();
-    },
-    {
-      message: 'Birth date must be in the past or today',
-    }
-  ),
-  nationality: z.string().min(1, {
-    message: 'Nationality is required',
-  }),
-  city: z.string().min(1, {
-    message: 'City is required',
-  }),
-  photo: z.instanceof(File, {
-    message: 'Photo is required',
-  }),
-  bio: z.string().min(1, {
-    message: 'Bio is required',
-  }),
-});
+    photo: z.instanceof(File, {
+      message: 'Photo is required',
+    }),
+    bio: z.string().min(1, {
+      message: 'Bio is required',
+    }),
+  });
 
 export const roleFormSchema = () =>
   z.object({
