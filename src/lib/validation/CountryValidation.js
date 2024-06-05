bbd62e55f6228636564e4bb11e47f3a5f85204b5
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-05 17:33:16 +0200

    Refactor countryFormSchema in CountryValidation.js

--- src/lib/validation/CountryValidation.js
+++ src/lib/validation/CountryValidation.js
@@ -1,29 +1,13 @@
 import * as z from 'zod';
 
-export const countryFormSchema = (isEdit) =>
+export const countryFormSchema = () =>
   z.object({
     name: z
       .string()
-      .min(1, {
+      .min(4, {
         message: 'Country name must be at least 4 characters.',
       })
       .max(30, {
         message: 'Country name must not be longer than 30 characters.',
-      })
-      .refine(
-        async (value) => {
-          const res = await fetch('/api/admin/country/check', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name: value, isEdit }),
-          });
-          const data = await res.json();
-          return data.success;
-        },
-        {
-          message: 'Country name already exists.',
-        }
-      ),
+      }),
   });
