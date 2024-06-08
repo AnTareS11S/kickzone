86d0ff3f400720b6c5cd7ece3ffac8aaf3a1ffcc
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-08 16:22:37 +0200

    Refactor layout component to use Sidebar instead of LeftSidebar and remove Bottombar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,21 +1,20 @@
 import Header from '../components/Header';
-import Bottombar from '../components/home/Bottombar';
-import LeftSidebar from '../components/home/LeftSidebar';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/home/Sidebar';
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <main className='flex flex-row'>
-        <LeftSidebar />
+        <Sidebar />
         <section className='main-container'>
           <div className='w-full'>
             <Outlet />
           </div>
         </section>
       </main>
-      <Bottombar />
+      <Sidebar />
     </div>
   );
 };
