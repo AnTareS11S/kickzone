176449fa0716793f506ca398ef3c11e1229e552f
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-13 19:06:02 +0200

    Refactor LeagueCard component to add hover effect

--- src/components/home/leagues/LeagueCard.jsx
+++ src/components/home/leagues/LeagueCard.jsx
@@ -5,7 +5,10 @@ const LeagueCard = ({ data }) => {
   return (
     <article className='flex flex-wrap gap-7'>
       {data.map((league) => (
-        <Card key={league._id} className='w-[250px] h-[100px] max-sm:w-full'>
+        <Card
+          key={league._id}
+          className='w-[250px] h-[100px] max-sm:w-full hover:bg-purple-200 transition duration-300 ease-in-out'
+        >
           <Link to={`/league/${league._id}`}>
             <CardHeader className='items-center pt-10'>
               <CardTitle>{league.name}</CardTitle>
