2a9c6cfb82d497f6d58d20fcc05d3b1e8d1ccc4b
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-13 16:33:49 +0200

    Refactor function to improve performance and readability

--- src/components/home/team/TeamStats.jsx
+++ src/components/home/team/TeamStats.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { FaRegCalendarAlt } from 'react-icons/fa';
-import { TbRectangleVerticalFilled } from 'react-icons/tb';
+import {
+  FaCheckCircle,
+  FaRegCalendarAlt,
+  FaSquare,
+  FaTimesCircle,
+  FaHandshake,
+} from 'react-icons/fa';
 import { PiSoccerBallFill } from 'react-icons/pi';
 import Spinner from '../../Spinner';
 import { useFetchSeasons } from '../../hooks/useFetchSeasons';
@@ -13,16 +18,39 @@ import {
   SelectValue,
 } from '../../ui/select';
 
+const fetchData = async (url, setter, setLoading) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message);
+    }
+
+    setter(data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+};
+
 const TeamStats = () => {
-  const teamId = useParams().id;
-  const [redCards, setRedCards] = useState([]);
-  const [yellowCards, setYellowCards] = useState([]);
-  const [goalsScored, setGoalsScored] = useState([]);
-  const [goalsLost, setGoalsLost] = useState([]);
+  const { id: teamId } = useParams();
+  const [stats, setStats] = useState({
+    redCards: [],
+    yellowCards: [],
+    goalsScored: [],
+    goalsLost: [],
+    wins: null,
+    draws: null,
+    losses: null,
+  });
   const seasons = useFetchSeasons();
   const [selectedSeason, setSelectedSeason] = useState('');
   const [loading, setLoading] = useState(true);
 
+  // Fetch initial season
   useEffect(() => {
     if (seasons.length > 0) {
       const lastSeason = seasons[seasons.length - 1].split(':')[1];
@@ -30,49 +58,48 @@ const TeamStats = () => {
     }
   }, [seasons]);
 
+  // Fetch team stats when season or teamId changes
   useEffect(() => {
-    const getTeamCards = async () => {
-      try {
-        const res = await fetch(
-          `/api/team/team-cards/${teamId}?season=${selectedSeason}`
-        );
-        const data = await res.json();
+    if (selectedSeason) {
+      setLoading(true);
 
-        if (!res.ok) {
-          throw new Error(data.message);
-        }
+      fetchData(
+        `/api/team/team-cards/${teamId}?season=${selectedSeason}`,
+        (data) => {
+          setStats((prev) => ({
+            ...prev,
+            redCards: data.redCards,
+            yellowCards: data.yellowCards,
+          }));
+        },
+        setLoading
+      );
 
-        setRedCards(data.redCards);
-        setYellowCards(data.yellowCards);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getTeamCards();
-  }, [teamId, selectedSeason]);
+      fetchData(
+        `/api/team/team-goals/${teamId}?season=${selectedSeason}`,
+        (data) => {
+          setStats((prev) => ({
+            ...prev,
+            goalsScored: data.goalsScored,
+            goalsLost: data.goalsLost,
+          }));
+        },
+        setLoading
+      );
 
-  useEffect(() => {
-    const getTeamGoals = async () => {
-      try {
-        const res = await fetch(
-          `/api/team/team-goals/${teamId}?season=${selectedSeason}`
-        );
-        const data = await res.json();
-
-        if (!res.ok) {
-          throw new Error(data.message);
-        }
-
-        setGoalsScored(data.goalsScored);
-        setGoalsLost(data.goalsLost);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getTeamGoals();
+      fetchData(
+        `/api/team/team-outcomes/${teamId}?season=${selectedSeason}`,
+        (data) => {
+          setStats((prev) => ({
+            ...prev,
+            wins: data.wins,
+            draws: data.draws,
+            losses: data.losses,
+          }));
+        },
+        setLoading
+      );
+    }
   }, [teamId, selectedSeason]);
 
   if (loading) {
@@ -80,7 +107,7 @@ const TeamStats = () => {
   }
 
   return (
-    <div className='max-w-4xl mx-auto py-12'>
+    <div className='max-w-6xl mx-auto py-12'>
       <h2 className='text-3xl font-bold text-gray-800 mb-8 text-center'>
         Team Statistics
       </h2>
@@ -113,66 +140,80 @@ const TeamStats = () => {
         </div>
         <div className='mb-8'>
           <h4 className='text-xl font-semibold text-gray-700'>
-            {yellowCards[0]?.league} - {yellowCards[0]?.season}
+            {stats.yellowCards[0]?.league} - {stats.yellowCards[0]?.season}
           </h4>
         </div>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8 my-8'>
+          <StatCard
+            title='Wins'
+            value={stats.wins}
+            icon={<FaCheckCircle className='text-green-500' />}
+            color='green-500'
+          />
+          <StatCard
+            title='Draws'
+            value={stats.draws}
+            icon={<FaHandshake className='text-blue-500' />}
+            color='blue-500'
+          />
+          <StatCard
+            title='Losses'
+            value={stats.losses}
+            icon={<FaTimesCircle className='text-gray-500' />}
+            color='gray-500'
+          />
+        </div>
         <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-8'>
-          <div>
-            <div className='flex items-center mb-4'>
-              <PiSoccerBallFill className='h-5 w-5 text-black mr-2' />
-              <h4 className='text-xl font-semibold text-gray-800'>
-                Goals Scored
-              </h4>
-            </div>
-            {goalsScored?.map((goal, index) => (
-              <div key={index} className='mb-4'>
-                <p className='text-lg text-gray-700'>{goal?.count} Goals</p>
-              </div>
-            ))}
-          </div>
-          <div>
-            <div className='flex items-center mb-4'>
-              <PiSoccerBallFill className='h-5 w-5 text-red-500 mr-2' />
-              <h4 className='text-xl font-semibold text-gray-800'>
-                Goals Lost
-              </h4>
-            </div>
-            {goalsLost?.map((goal, index) => (
-              <div key={index} className='mb-4'>
-                <p className='text-lg text-gray-700'>{goal?.count} Goals</p>
-              </div>
-            ))}
-          </div>
-          <div>
-            <div className='flex items-center mb-4'>
-              <TbRectangleVerticalFilled className='h-5 w-5 text-yellow-500 mr-2' />
-              <h4 className='text-xl font-semibold text-gray-800'>
-                Yellow Cards
-              </h4>
-            </div>
-            {yellowCards?.map((card, index) => (
-              <div key={index} className='mb-4'>
-                <p className='text-lg text-gray-700'>
-                  {card?.count} Yellow Cards
-                </p>
-              </div>
-            ))}
-          </div>
-          <div>
-            <div className='flex items-center mb-4'>
-              <TbRectangleVerticalFilled className='h-5 w-5 text-red-500 mr-2' />
-              <h4 className='text-xl font-semibold text-gray-800'>Red Cards</h4>
-            </div>
-            {redCards?.map((card, index) => (
-              <div key={index} className='mb-4'>
-                <p className='text-lg text-gray-700'>{card?.count} Red Cards</p>
-              </div>
-            ))}
-          </div>
+          <DetailCard
+            title='Goals Scored'
+            items={stats.goalsScored}
+            icon={<PiSoccerBallFill className='text-black' />}
+          />
+          <DetailCard
+            title='Goals Lost'
+            items={stats.goalsLost}
+            icon={<PiSoccerBallFill className='text-red-500' />}
+          />
+          <DetailCard
+            title='Yellow Cards'
+            items={stats.yellowCards}
+            icon={<FaSquare className='text-yellow-500' />}
+          />
+          <DetailCard
+            title='Red Cards'
+            items={stats.redCards}
+            icon={<FaSquare className='text-red-500' />}
+          />
         </div>
       </div>
     </div>
   );
 };
+
+const StatCard = ({ title, value, icon, color }) => (
+  <div className={`bg-gray-100 p-6 rounded-lg shadow-md text-${color}`}>
+    <div className='flex items-center mb-4'>
+      {icon}
+      <h4 className='text-xl font-semibold text-gray-800 ml-2'>{title}</h4>
+    </div>
+    <p className={`text-4xl font-bold text-${color}`}>{value}</p>
+  </div>
+);
+
+const DetailCard = ({ title, items, icon }) => (
+  <div>
+    <div className='flex items-center mb-4'>
+      {icon}
+      <h4 className='text-xl font-semibold text-gray-800 ml-2'>{title}</h4>
+    </div>
+    {items?.map((item, index) => (
+      <div key={index} className='mb-4'>
+        <p className='text-lg text-gray-700'>
+          {item?.count} {title}
+        </p>
+      </div>
+    ))}
+  </div>
+);
 
 export default TeamStats;
