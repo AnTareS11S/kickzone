f7daad9a16c6f1863de65c7e969b5ab3b47fa8db
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-13 17:35:09 +0200

    Refactor TeamResult component to use useState and useEffect hooks

--- src/components/home/team/TeamResult.jsx
+++ src/components/home/team/TeamResult.jsx
@@ -1,12 +1,56 @@
 import { Link, useParams } from 'react-router-dom';
 import { Card, CardContent } from '../../ui/card';
-import { useFetchResultsAndMatchesByTeamId } from '../../hooks/useFetchResultsAndMatchesByTeamId';
 import Spinner from '../../Spinner';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../../ui/select';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { useFetchSeasons } from '../../hooks/useFetchSeasons';
 
 const TeamResult = () => {
   const teamId = useParams().id;
-  const { results, matches, loading } =
-    useFetchResultsAndMatchesByTeamId(teamId);
+  const [results, setResults] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const seasons = useFetchSeasons();
+  const [selectedSeason, setSelectedSeason] = useState('');
+
+  useEffect(() => {
+    if (seasons.length > 0) {
+      const lastSeason = seasons[seasons.length - 1].split(':')[1];
+      setSelectedSeason(lastSeason);
+    }
+  }, [seasons]);
+
+  useEffect(() => {
+    const fetchResultsData = async () => {
+      try {
+        const res = await fetch(
+          `/api/team/results/${teamId}?season=${selectedSeason}`
+        );
+        if (!res.ok) {
+          throw new Error('Failed to fetch results data!');
+        }
+        const data = await res.json();
+
+        setResults(data.results);
+        setMatches(data.matches);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchResultsData();
+  }, [teamId, selectedSeason]);
 
   if (loading) {
     return <Spinner />;
@@ -14,6 +58,29 @@ const TeamResult = () => {
 
   return (
     <>
+      <div className='flex justify-end items-center mb-8'>
+        <div className='flex items-center space-x-2'>
+          <Select
+            value={selectedSeason}
+            onValueChange={(value) => {
+              setSelectedSeason(value);
+              setLoading(true);
+            }}
+          >
+            <SelectTrigger className='flex items-center border border-gray-300 rounded-lg px-4 py-2 hover:border-gray-500 bg-white shadow-sm'>
+              <SelectValue placeholder='Select Season' />
+            </SelectTrigger>
+            <SelectContent>
+              {seasons?.map((season, index) => (
+                <SelectItem key={index} value={season?.split(':')[1]}>
+                  {season?.split(':')[0]}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FaRegCalendarAlt className='text-gray-500' />
+        </div>
+      </div>
       {matches?.length > 0 ? (
         <Card className='mb-5 mt-5'>
           <CardContent className='p-6'>
