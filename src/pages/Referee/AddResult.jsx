9f3556f9d297e16046d4bf7a706cc3fefe5ac689
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-18 11:26:33 +0200

    Update match details display in AddResult component

--- src/pages/Referee/AddResult.jsx
+++ src/pages/Referee/AddResult.jsx
@@ -112,7 +112,12 @@ const AddResult = () => {
         <h2 className='text-2xl font-semibold text-gray-800 mb-2'>
           Match Details
         </h2>
-        <p className='text-sm text-gray-600 mb-6'>Add match result here.</p>
+        <div className='text-gray-600 flex flex-row justify-between'>
+          <p>Add match result here.</p>
+          <p>
+            {match?.league?.name} / {match?.season?.name}
+          </p>
+        </div>
       </div>
       <Separator />
       <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mb-8'>
