9e6d31b61c9c45ed00a7c5ad2b8ea1228def14e1
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-08-21 15:49:00 +0200

    Refactor code

--- src/pages/Referee/AddResult.jsx
+++ src/pages/Referee/AddResult.jsx
@@ -1,46 +1,25 @@
 import { useParams } from 'react-router-dom';
-import { Separator } from '../../components/ui/separator';
-import BackButton from '../../components/BackButton';
-import { useFetchMatchById } from '../../components/hooks/useFetchMatchById';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form } from '../../components/ui/form';
-import FormArea from '../../components/FormArea';
 import { Button } from '../../components/ui/button';
-import { resultFormSchema } from '../../lib/validation/ResultValidation';
-import CrudPanel from '../../components/CrudPanel';
+import { Separator } from '../../components/ui/separator';
 import { useToast } from '../../components/ui/use-toast';
-import { useFetchMatchResultById } from '../../components/hooks/useFetchMatchResultById';
-import { useEffect } from 'react';
+import BackButton from '../../components/BackButton';
+import FormArea from '../../components/FormArea';
+import CrudPanel from '../../components/CrudPanel';
 import Spinner from '../../components/Spinner';
+import { useFetchMatchById } from '../../components/hooks/useFetchMatchById';
+import { useFetchMatchResultById } from '../../components/hooks/useFetchMatchResultById';
+import { resultFormSchema } from '../../lib/validation/ResultValidation';
 
 const columns = [
-  {
-    name: 'No.',
-    selector: (row, index) => index + 1,
-    grow: 0,
-  },
-  {
-    name: 'Name',
-    selector: (row) => row?.name,
-    sortable: true,
-  },
-  {
-    name: 'Surname',
-    selector: (row) => row?.surname,
-    sortable: true,
-  },
-  {
-    name: 'Age',
-    selector: (row) => row?.age,
-    sortable: true,
-    hide: 'max-md',
-  },
-  {
-    name: 'Pos',
-    selector: (row) => row?.positionShortcut,
-    sortable: true,
-  },
+  { name: 'No.', selector: (row, index) => index + 1, grow: 0 },
+  { name: 'Name', selector: (row) => row?.name, sortable: true },
+  { name: 'Surname', selector: (row) => row?.surname, sortable: true },
+  { name: 'Age', selector: (row) => row?.age, sortable: true, hide: 'sm' },
+  { name: 'Pos', selector: (row) => row?.positionShortcut, sortable: true },
   {
     name: 'Number',
     selector: (row) => row?.number,
@@ -50,17 +29,14 @@ const columns = [
 ];
 
 const AddResult = () => {
-  const matchId = useParams().id;
+  const { id: matchId } = useParams();
   const { match, loading } = useFetchMatchById(matchId);
   const { result } = useFetchMatchResultById(matchId);
   const { toast } = useToast();
 
   const form = useForm({
     resolver: zodResolver(resultFormSchema()),
-    defaultValues: {
-      homeTeamScore: 0,
-      awayTeamScore: 0,
-    },
+    defaultValues: { homeTeamScore: 0, awayTeamScore: 0 },
     mode: 'onChange',
   });
 
@@ -74,14 +50,15 @@ const AddResult = () => {
   }, [result, form]);
 
   const onSubmit = async (data) => {
-    const updatedData = { ...data, match: match?._id, season: match?.season };
     try {
-      const res = await fetch(`/api/referee/add-result`, {
+      const res = await fetch('/api/referee/add-result', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedData),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          ...data,
+          match: match?._id,
+          season: match?.season,
+        }),
       });
 
       if (res.ok) {
@@ -97,118 +74,115 @@ const AddResult = () => {
         });
       }
     } catch (error) {
-      console.log(error);
+      console.error('Error adding match result:', error);
     }
   };
 
-  if (loading) {
-    return <Spinner />;
-  }
+  if (loading) return <Spinner />;
 
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
       <BackButton />
-      <div className='mt-8'>
-        <h2 className='text-2xl font-semibold text-gray-800 mb-2'>
-          Match Details
-        </h2>
-        <div className='text-gray-600 flex flex-row justify-between'>
-          <p>Add match result here.</p>
-          <p>
-            {match?.league?.name} / {match?.season?.name}
-          </p>
-        </div>
-      </div>
-      <Separator />
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mb-8'>
-        <div>
-          <p className='text-lg font-semibold text-gray-800 mb-2'>Match Date</p>
-          <p className='text-gray-600'>
-            {new Date(match.startDate).toLocaleString('en-US', {
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric',
-              hour: 'numeric',
-              minute: 'numeric',
-            })}
-          </p>
-        </div>
-        <div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className='flex flex-col items-end max-sm:items-start max-md:items-start'>
+      <div className='mt-8 bg-white shadow-lg rounded-lg overflow-hidden'>
+        <div className='px-6 py-8'>
+          <h2 className='text-3xl font-bold text-gray-900 mb-4'>
+            Match Details
+          </h2>
+          <div className='flex flex-col sm:flex-row justify-between text-gray-600 mb-6'>
+            <p className='mb-2 sm:mb-0'>Add match result here.</p>
+            <p>
+              {match?.league?.name} / {match?.season?.name}
+            </p>
+          </div>
+          <Separator className='my-6' />
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mb-8'>
+            <div>
+              <p className='text-lg font-semibold text-gray-800 mb-2'>
+                Match Date
+              </p>
+              <p className='text-gray-600'>
+                {new Date(match.startDate).toLocaleString('en-US', {
+                  day: 'numeric',
+                  month: 'long',
+                  year: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                })}
+              </p>
+            </div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-4'
+              >
                 <FormArea
                   id='homeTeamScore'
                   label={`${match?.homeTeam?.name} Score`}
                   type='number'
                   form={form}
                   name='homeTeamScore'
-                  className='w-auto'
                 />
-
                 <FormArea
                   id='awayTeamScore'
                   label={`${match?.awayTeam?.name} Score`}
                   type='number'
                   form={form}
                   name='awayTeamScore'
-                  className='w-auto'
                 />
-              </div>
-              <div className='flex flex-col md:flex-row justify-end max-sm:w-fit max-md:w-fit'>
                 <Button
                   type='submit'
                   disabled={!form.formState.isValid}
-                  className='w-auto bg-primary-500 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded mt-4 md:mt-0'
+                  className='w-full sm:w-auto bg-primary-500 hover:bg-primary-600 text-white font-semibold py-2 px-6 rounded-md transition duration-300'
                 >
-                  Save
+                  Save Result
                 </Button>
-              </div>
-            </form>
-          </Form>
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
-      <Separator />
-      <div className='mt-8'>
-        <h2 className='text-2xl font-semibold text-gray-800 mb-2'>
-          Player Stats
-        </h2>
-        <p className='text-sm text-gray-600 mb-6'>
-          Add player stats for this match.
-        </p>
+
+      <div className='mt-12 bg-white shadow-lg rounded-lg overflow-hidden'>
+        <div className='px-6 py-8'>
+          <h2 className='text-3xl font-bold text-gray-900 mb-4'>
+            Player Stats
+          </h2>
+          <p className='text-sm text-gray-600 mb-6'>
+            Add player stats for this match.
+          </p>
+          <Separator className='my-6' />
+          {match?.homeTeam && (
+            <div className='mb-8'>
+              <h3 className='text-2xl font-semibold text-gray-800 mb-4'>
+                {match.homeTeam.name} players:
+              </h3>
+              <CrudPanel
+                apiPath={`team-player/${match.homeTeam._id}`}
+                columns={columns}
+                title='Player'
+                formSchema={resultFormSchema}
+                isAction={false}
+                isExpandable={true}
+              />
+            </div>
+          )}
+          {match?.awayTeam && (
+            <div className='mt-8'>
+              <h3 className='text-2xl font-semibold text-gray-800 mb-4'>
+                {match.awayTeam.name} players:
+              </h3>
+              <CrudPanel
+                apiPath={`team-player/${match.awayTeam._id}`}
+                columns={columns}
+                title='Player'
+                formSchema={resultFormSchema}
+                isAction={false}
+                isExpandable={true}
+              />
+            </div>
+          )}
+        </div>
       </div>
-      <Separator />
-      {match?.homeTeam ? (
-        <div className='mt-8'>
-          <h3 className='text-xl font-semibold text-gray-800 mb-4'>
-            {match?.homeTeam?.name} players:
-          </h3>
-          <CrudPanel
-            apiPath={`team-player/${match?.homeTeam?._id}`}
-            columns={columns}
-            title='Player'
-            formSchema={resultFormSchema}
-            isAction={false}
-            isExpandable={true}
-          />
-        </div>
-      ) : null}
-      <Separator />
-      {match?.awayTeam ? (
-        <div className='mt-8'>
-          <h3 className='text-xl font-semibold text-gray-800 mb-4'>
-            {match?.awayTeam?.name} players:
-          </h3>
-          <CrudPanel
-            apiPath={`team-player/${match?.awayTeam?._id}`}
-            columns={columns}
-            title='Player'
-            formSchema={resultFormSchema}
-            isAction={false}
-            isExpandable={true}
-          />
-        </div>
-      ) : null}
     </div>
   );
 };
