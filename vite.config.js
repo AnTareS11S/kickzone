331cbebfdda4733ca85055388d862d10273880c7
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-02 18:19:19 +0200

    Refactor build configuration to optimize chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,19 @@ export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
