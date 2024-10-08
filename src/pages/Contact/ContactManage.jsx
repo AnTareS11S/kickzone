a4afb937fff7733dbad51e642436e314883f8d78
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-08 16:09:46 +0200

    Add ContactManage component to manage contact information on the Admin Dashboard

---
+++ src/pages/Contact/ContactManage.jsx
@@ -0,0 +1,114 @@
+import BackButton from '../../components/BackButton';
+import CrudPanel from '../../components/CrudPanel';
+import DeleteEntity from '../../components/DeleteEntity';
+import EditEntity from '../../components/EditEntity';
+import { Separator } from '../../components/ui/separator';
+import { z } from 'zod';
+
+const contactSchema = () =>
+  z.object({
+    name: z.string().min(3, 'Name is required'),
+    lastName: z.string().min(3, 'Last name is required'),
+    email: z.string().email('Invalid email'),
+    phone: z.string().min(3, 'Phone is required'),
+    address: z.string().min(3, 'Address is required'),
+    active: z.boolean(),
+  });
+
+const ContactManage = () => {
+  const columns = [
+    {
+      name: 'No.',
+      selector: (row, index) => index + 1,
+      grow: 0,
+    },
+    {
+      name: 'Name',
+      selector: (row) => row.name + ' ' + row.lastName,
+      sortable: true,
+    },
+    {
+      name: 'E-mail',
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: 'Active',
+      selector: (row) => (row.active ? 'Yes' : 'No'),
+      sortable: true,
+    },
+  ];
+
+  const fields = [
+    {
+      id: 'name',
+      label: 'Name',
+      type: 'text',
+      name: 'name',
+    },
+    {
+      id: 'lastName',
+      label: 'Last Name',
+      type: 'text',
+      name: 'lastName',
+    },
+    {
+      id: 'email',
+      label: 'E-mail',
+      type: 'email',
+      name: 'email',
+    },
+    {
+      id: 'phone',
+      label: 'Phone',
+      type: 'text',
+      name: 'phone',
+    },
+    {
+      id: 'address',
+      label: 'Address',
+      type: 'text',
+      name: 'address',
+    },
+    {
+      id: 'active',
+      label: 'Active',
+      type: 'checkbox',
+      name: 'active',
+    },
+  ];
+
+  return (
+    <div className='space-y-6'>
+      <BackButton />
+      <div>
+        <div className='text-heading2-bold'>Contact</div>
+        <p className='text-sm text-muted-foreground'>
+          Manage contact information.
+        </p>
+      </div>
+      <Separator />
+      <CrudPanel
+        apiPath='contact'
+        columns={columns}
+        fields={fields}
+        title='Contact'
+        onEditComponent={EditEntity}
+        onDeleteComponent={DeleteEntity}
+        formSchema={contactSchema}
+        defaultValues={{
+          name: '',
+          lastName: '',
+          email: '',
+          phone: '',
+          address: '',
+          active: true,
+        }}
+        isExpandable={false}
+        isAction={true}
+      />
+    </div>
+  );
+};
+
+export default ContactManage;
