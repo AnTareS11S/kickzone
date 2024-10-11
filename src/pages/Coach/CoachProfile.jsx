61fd148d4a8fbdc9c2ab999bc1539804c16f07c8
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-11 15:18:29 +0200

    Refactor CoachProfile component to match new file structure and remove unused code

---
+++ src/pages/Coach/CoachProfile.jsx
@@ -0,0 +1,23 @@
+import CoachForm from '../../components/coach/CoachForm';
+import { Separator } from '../../components/ui/separator';
+
+import { useSelector } from 'react-redux';
+
+const CoachProfile = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className='space-y-6 max-w-screen-md mx-auto'>
+      <div>
+        <h3 className='text-lg font-medium'>Coach Profile</h3>
+        <p className='text-sm text-muted-foreground'>
+          Fulfill your coach profile
+        </p>
+      </div>
+      <Separator />
+      <CoachForm currentUser={currentUser} />
+    </div>
+  );
+};
+
+export default CoachProfile;
