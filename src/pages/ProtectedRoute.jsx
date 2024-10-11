9330e488c3d410e63b87c007d912edc71e4a632d
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-11 14:19:15 +0200

    Refactor ProtectedRoute component to fetch user data and add loading spinner

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,15 +1,36 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchUserData } from '../redux/user/userSlice';
+import Spinner from '../components/Spinner';
 
 const ProtectedRoute = () => {
-  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { currentUser, loading } = useSelector((state) => state.user);
   const isAuthenticated = !!currentUser;
 
-  return isAuthenticated && currentUser.isOnboardingCompleted ? (
-    <Navigate to='/onboarding' />
-  ) : (
-    <Outlet />
-  );
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(fetchUserData(currentUser._id));
+    }
+  }, [dispatch, isAuthenticated, currentUser]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (!isAuthenticated) {
+    // If user is not authenticated, redirect to login page
+    return <Navigate to='/sign-in' />;
+  }
+
+  if (isAuthenticated && !currentUser.isOnboardingCompleted) {
+    // If user is authenticated but hasn't completed onboarding, redirect to onboarding page
+    return <Navigate to='/onboarding' />;
+  }
+
+  // If user is authenticated and has completed onboarding, render the protected route
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
