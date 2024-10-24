da174b42b406a200d65d58af052a16240e02cd62
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-24 14:45:44 +0200

    Refactor SocketContext to provide socket functionality using a custom hook

---
+++ src/contexts/SocketContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext } from 'react';
+import { useSocket } from '../hook/useSocket';
+
+const SocketContext = createContext(null);
+
+export const SocketProvider = ({ children }) => {
+  const socket = useSocket();
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+export const useSocketContext = () => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error('useSocketContext must be used within a SocketProvider');
+  }
+
+  return context;
+};
