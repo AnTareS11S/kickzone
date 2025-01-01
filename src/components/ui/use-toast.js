c85ecbdd287cc20d5dbf64753285d6c39f2bccff
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-01 17:22:06 +0100

    Enhance use-toast component: increase toast limit and reduce remove delay

--- src/components/ui/use-toast.js
+++ src/components/ui/use-toast.js
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from 'react';
 
-const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_LIMIT = 2;
+const TOAST_REMOVE_DELAY = 100;
 
 const actionTypes = {
   ADD_TOAST: 'ADD_TOAST',
