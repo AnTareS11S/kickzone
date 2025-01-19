669690ceacd2eb1c27919632165bf76316258c4d
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-19 16:10:41 +0100

    Update ThreadList component to include user role in forum links and display creation time

--- src/components/forum/ThreadList.jsx
+++ src/components/forum/ThreadList.jsx
@@ -6,12 +6,16 @@ import { Avatar, AvatarImage } from '../ui/avatar';
 import { getTimeAgo } from '../../lib/utils';
 import { Link } from 'react-router-dom';
 
-const ThreadList = ({ threads }) => {
+const ThreadList = ({ threads, role }) => {
   return (
     <div className='lg:col-span-3 space-y-4'>
       {threads?.map((thread) => (
         <Card key={thread?._id} className='hover:shadow-md transition-shadow'>
-          <Link to={`/forum/${thread?._id}`}>
+          <Link
+            to={`/dashboard/${role === 'Player' ? 'player' : 'coach'}/forum/${
+              thread?._id
+            }`}
+          >
             <CardContent className='p-6 '>
               <div className='flex items-start gap-4 '>
                 <Avatar className='h-10 w-10'>
@@ -49,7 +53,7 @@ const ThreadList = ({ threads }) => {
                     </div>
                     <div className='flex items-center gap-1'>
                       <FaClock className='h-4 w-4' />
-                      {getTimeAgo(thread?.updatedAt)}
+                      {getTimeAgo(thread?.createdAt)}
                     </div>
                   </div>
                 </div>
