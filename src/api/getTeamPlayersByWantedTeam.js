be86d34550012480c8dba931e01bae5da4735736
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-20 19:59:40 +0100

    Refactor GetTeamPlayersByWantedTeam to use objects for players with id and name properties

--- src/api/getTeamPlayersByWantedTeam.js
+++ src/api/getTeamPlayersByWantedTeam.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 
 export const GetTeamPlayersByWantedTeam = (id) => {
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState([
+    {
+      id: '',
+      name: '',
+    },
+  ]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -10,7 +15,12 @@ export const GetTeamPlayersByWantedTeam = (id) => {
       try {
         const res = await fetch(`/api/player/get-wanted-team/${id}`);
         const data = await res.json();
-        setPlayers(data);
+        setPlayers(
+          data.map((player) => ({
+            id: player._id,
+            name: player.name + ' ' + player.surname,
+          }))
+        );
         setLoading(false);
       } catch (error) {
         setError(error);
