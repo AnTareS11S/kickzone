babc2cb77d84e71d1f9855c47882c01a94786ab2
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-21 13:26:59 +0100

    Refactor GetTeamForumCategories to include currentUser and enhance category fetching logic

--- src/api/getTeamForumCategories.js
+++ src/api/getTeamForumCategories.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export const GetTeamForumCategories = (isChanged) => {
-  const [categories, setCategories] = useState([]);
-  const [categoriesToSelect, setCategoriesToSelect] = useState([
+  const { currentUser } = useSelector((state) => state.user);
+  const [categories, setCategories] = useState([
     {
       id: '',
       name: '',
@@ -15,14 +16,53 @@ export const GetTeamForumCategories = (isChanged) => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const res = await fetch(`/api/admin/team-forum-categories`);
+        const res = await fetch(
+          `/api/admin/team-forum-categories/${currentUser?._id}/${currentUser?.role}`
+        );
         const data = await res.json();
-        setCategories(data);
-        setCategoriesToSelect(
+        setCategories(
           data?.map((category) => ({
             id: category?._id,
             name: category?.name,
             count: category?.count,
+          }))
+        );
+
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, [isChanged, currentUser]);
+
+  return { categories, loading, error };
+};
+
+export const GetTeamForumCategoriesToSelect = () => {
+  const [categoriesToSelect, setCategoriesToSelect] = useState([
+    {
+      id: '',
+      name: '',
+    },
+  ]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch(`/api/admin/team-forum-categories`);
+        const data = await res.json();
+
+        setCategoriesToSelect(
+          data?.map((category) => ({
+            id: category?._id,
+            name: category?.name,
           }))
         );
         setLoading(false);
@@ -35,7 +75,7 @@ export const GetTeamForumCategories = (isChanged) => {
     };
 
     fetchCategories();
-  }, [isChanged]);
+  }, []);
 
-  return { categories, categoriesToSelect, loading, error };
+  return { categoriesToSelect, loading, error };
 };
