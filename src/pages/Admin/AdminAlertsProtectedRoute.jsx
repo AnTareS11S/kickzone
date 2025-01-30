75b4b5d51c633ad392e2c82240a5af9f5061b3ac
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-30 16:38:47 +0100

    Add AdminAlertsProtectedRoute component for user role change notification protection

---
+++ src/pages/Admin/AdminAlertsProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const AdminAlertsProtectedRoute = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  console.log(currentUser);
+
+  if (!currentUser || currentUser?.isRoleChangeNotificationRead) {
+    return <Navigate to='/' replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AdminAlertsProtectedRoute;
