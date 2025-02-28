50c44b1b5ad1604a2b6352ca34f8d26b1914c836
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-02-28 18:40:56 +0100

    Disable dropdown trigger when downloading to improve user experience

--- src/pages/Referee/MatchDetails.jsx
+++ src/pages/Referee/MatchDetails.jsx
@@ -188,7 +188,10 @@ const MatchDetails = () => {
                 </p>
               </div>
               <DropdownMenu>
-                <DropdownMenuTrigger className='inline-flex items-center px-4 py-2 bg-primary-500 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
+                <DropdownMenuTrigger
+                  disabled={isDownloading}
+                  className='inline-flex items-center px-4 py-2 bg-primary-500 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+                >
                   {isDownloading ? (
                     <>
                       <span>Downloading...</span>
